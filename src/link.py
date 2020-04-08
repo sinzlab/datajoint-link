11a74310c86243118373aa9d78c8836b5eaf2b92
christoph <chris24.blessing@gmail.com>
2020-04-08 12:31:53 +0200

    Implement initial version of pull prototype

---
+++ src/link.py
@@ -0,0 +1,130 @@
+from itertools import product, chain
+from contextlib import contextmanager
+
+import datajoint as dj
+
+
+class Link:
+    def __init__(self, local_schema, remote_schema):
+        self.local_schema = local_schema
+        self.remote_schema = remote_schema
+        self.local_conn = dj.Connection(local_schema.connection.conn_info["host"], "datajoint", "datajoint")
+        self.remote_conn = self.remote_schema.connection
+        self.remote_table = None
+        self.outbound_table = None
+        self.inbound_table = None
+        self.local_table = None
+
+    def __call__(self, table_cls):
+        self.set_up_remote_table(table_cls)
+        self.set_up_outbound_table(table_cls)
+        self.set_up_inbound_table(table_cls)
+        self.set_up_local_table(table_cls)
+        return self.local_table
+
+    def set_up_remote_table(self, table_cls):
+        remote_tables = dict()
+        self.remote_schema.spawn_missing_classes(context=remote_tables)
+        self.remote_table = remote_tables[table_cls.__name__]
+
+    def set_up_outbound_table(self, table_cls):
+        class OutboundTable(dj.Lookup):
+            remote_table = self.remote_table
+            definition = """
+            remote_host: varchar(64)
+            remote_schema: varchar(64)
+            -> self.remote_table
+            """
+
+        OutboundTable.__name__ = table_cls.__name__ + "Outbound"
+        outbound_schema = dj.schema("datajoint_outbound__" + self.remote_schema.database, connection=self.remote_conn)
+        self.outbound_table = outbound_schema(OutboundTable)
+
+    def set_up_inbound_table(self, table_cls):
+        class InboundTable(dj.Lookup):
+            definition = str(self.outbound_table().heading)
+
+        InboundTable.__name__ = table_cls.__name__ + "Inbound"
+        inbound_schema = dj.schema("datajoint_inbound__" + self.local_schema.database, connection=self.local_conn)
+        self.inbound_table = inbound_schema(InboundTable)
+
+    def set_up_local_table(self, table_cls):
+        class LocalTable(dj.Lookup):
+            link = self
+            remote = self.remote_table()
+            definition = """
+            -> self.link.inbound_table
+            """
+
+            def pull(self, restriction=None):
+                self.link.pull(restriction=restriction)
+
+            def delete(self, verbose=True):
+                super().delete(verbose=verbose)
+                self.link.refresh()
+
+            def delete_quick(self, get_count=False):
+                super().delete_quick(get_count=get_count)
+                self.link.refresh()
+
+        LocalTable.__name__ = table_cls.__name__
+        heading = self.remote_table().heading
+        secondary = (a for a, n in product(str(heading).split("\n"), heading.secondary_attributes) if a.startswith(n))
+        LocalTable.definition = "\n".join(chain([LocalTable.definition], secondary))
+        self.local_table = self.local_schema(LocalTable)
+
+    def refresh(self):
+        with self.transaction():
+            (self.inbound_table() - self.local_table()).delete_quick()
+            keys = self.inbound_table().fetch()
+            (self.outbound_table() - keys).delete_quick()
+
+    def pull(self, restriction=None):
+        if restriction is None:
+            restriction = dj.AndList()
+        self.refresh()
+        primary_keys = (self.remote_table().proj() & restriction).fetch(as_dict=True)
+        entities = (self.remote_table() & restriction).fetch(as_dict=True)
+        with self.transaction():
+            self.outbound_table().insert(
+                [
+                    dict(pk, remote_host=self.local_conn.conn_info["host"], remote_schema=self.local_schema.database)
+                    for pk in primary_keys
+                ],
+                skip_duplicates=True,
+            )
+            self.inbound_table().insert(
+                [
+                    dict(pk, remote_host=self.remote_conn.conn_info["host"], remote_schema=self.remote_schema.database)
+                    for pk in primary_keys
+                ],
+                skip_duplicates=True,
+            )
+            self.local_table().insert(
+                [
+                    dict(e, remote_host=self.remote_conn.conn_info["host"], remote_schema=self.remote_schema.database)
+                    for e in entities
+                ],
+                skip_duplicates=True,
+            )
+
+    @contextmanager
+    def transaction(self):
+        old_local_table_conn = self.local_table.connection
+        try:
+            self.remote_conn.start_transaction()
+            self.local_conn.start_transaction()
+            self.local_table.connection = self.local_conn
+            yield
+        except Exception:
+            self.remote_conn.cancel_transaction()
+            self.local_conn.cancel_transaction()
+            raise
+        else:
+            self.remote_conn.commit_transaction()
+            self.local_conn.commit_transaction()
+        finally:
+            self.local_table.connection = old_local_table_conn
+
+
+link = Link
