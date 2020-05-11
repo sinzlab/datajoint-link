9e956307a4f9db738eb6b275864b8f0fe220edcd
christoph <chris24.blessing@gmail.com>
2020-05-11 13:30:39 +0200

    Make it a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="link",
+    version="0.0.1",
+    author="Christoph Blessing",
+    author_email="chris24.blessing@gmail.com",
+    description="A package that lets you link DataJoint schemas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cblessing24/link",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent",],
+    python_requires=">=3.8",
+    install_requires=["datajoint"],
+)
