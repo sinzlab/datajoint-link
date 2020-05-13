64f474c84ed5dfff7ff3f659139095719deba058
christoph <chris24.blessing@gmail.com>
2020-05-13 17:08:14 +0200

    Add config fixture

--- tests/functional/test_database.py
+++ tests/functional/test_database.py
@@ -1,5 +1,6 @@
 import os
 import time
+from dataclasses import dataclass
 
 import pytest
 import docker
@@ -12,43 +13,68 @@ def docker_client():
 
 
 @pytest.fixture
-def source_database_container_name(docker_client):
-    name = os.environ.get("SOURCE_DATABASE_NAME", "test_source_database")
-    password = os.environ.get("SOURCE_MYSQL_ROOT_PASSWORD", "password")
+def config():
+    @dataclass
+    class Config:
+        network: str
+        health_check_start_period: float
+        health_check_max_retries: int
+        health_check_interval: float
+        remove_containers: bool
+        src_db_name: str
+        src_db_root_pass: str
+
+    return Config(
+        os.environ.get("DOCKER_NETWORK", "test_network"),
+        float(os.environ.get("HEALTH_CHECK_START_PERIOD", 0)),
+        int(os.environ.get("HEALTH_CHECK_MAX_RETRIES", 60)),
+        float(os.environ.get("HEALTH_CHECK_INTERVAL", 1)),
+        bool(int(os.environ.get("DOCKER_REMOVE_CONTAINERS", True))),
+        os.environ.get("SOURCE_DATABASE_NAME", "test_source_database"),
+        os.environ.get("SOURCE_MYSQL_ROOT_PASSWORD", "password"),
+    )
+
+
+@pytest.fixture
+def source_database_container_name(docker_client, config):
     container = None
     try:
         container = docker_client.containers.run(
             "datajoint/mysql:5.7",
             detach=True,
-            name=name,
-            environment=dict(MYSQL_ROOT_PASSWORD=password),
-            network=os.environ.get("DOCKER_NETWORK", "test_network"),
+            name=config.src_db_name,
+            environment=dict(MYSQL_ROOT_PASSWORD=config.src_db_root_pass),
+            network=config.network,
         )
-        time.sleep(float(os.environ.get("HEALTH_CHECK_START_PERIOD", 0)))
+        time.sleep(config.health_check_start_period)
         n_tries = 0
         while True:
             container.reload()
             if container.attrs["State"]["Health"]["Status"] == "healthy":
                 break
-            if n_tries >= int(os.environ.get("HEALTH_CHECK_MAX_RETRIES", 60)):
-                raise RuntimeError(f"Trying to bring up container '{name}' exceeded max number of retries")
-            time.sleep(float(os.environ.get("HEALTH_CHECK_INTERVAL", 1)))
+            if n_tries >= config.health_check_max_retries:
+                raise RuntimeError(
+                    f"Trying to bring up container '{config.src_db_name}' exceeded max number of retries"
+                )
+            time.sleep(config.health_check_interval)
             n_tries += 1
         yield container.name
     finally:
         if container is not None:
             container.stop()
-            if os.environ.get("DOCKER_REMOVE_CONTAINERS", True):
+            if config.remove_containers:
                 container.remove()
 
 
 @pytest.fixture
-def source_database_connection(source_database_container_name):
-    password = os.environ.get("SOURCE_MYSQL_ROOT_PASSWORD", "password")
+def source_database_connection(config, source_database_container_name):
     connection = None
     try:
         connection = pymysql.connect(
-            host=source_database_container_name, user="root", password=password, cursorclass=pymysql.cursors.DictCursor
+            host=source_database_container_name,
+            user="root",
+            password=config.src_db_root_pass,
+            cursorclass=pymysql.cursors.DictCursor,
         )
         yield connection
     finally:
