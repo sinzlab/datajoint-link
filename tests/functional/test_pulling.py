70f95b8cfc6c60c403d73b59e08ce32c58ac9895
christoph <chris24.blessing@gmail.com>
2020-05-26 13:41:33 +0200

    Use fixtures to control whether to test with or without part table and external

--- tests/functional/test_pulling.py
+++ tests/functional/test_pulling.py
@@ -41,142 +41,112 @@ def create_ext_files(src_temp_dir, ext_files):
 
 
 @pytest.fixture
-def get_src_data(ext_files, create_ext_files):
-    def _get_src_data(use_part_table, use_external):
-        def add_ext_files(kind):
-            create_ext_files(kind)
-            src_data[kind] = [dict(e, ext_attr=f) for e, f in zip(src_data[kind], ext_files[kind])]
+def src_data(use_part_table, use_external, ext_files, create_ext_files):
+    def add_ext_files(kind):
+        create_ext_files(kind)
+        src_data[kind] = [dict(e, ext_attr=f) for e, f in zip(src_data[kind], ext_files[kind])]
 
-        src_data = dict(master=[dict(prim_attr=i, sec_attr=-i) for i in range(10)])
+    src_data = dict(master=[dict(prim_attr=i, sec_attr=-i) for i in range(10)])
+    if use_part_table:
+        src_data["part"] = [dict(prim_attr=e["prim_attr"], sec_attr=i) for i, e in enumerate(src_data["master"])]
+    if use_external:
+        add_ext_files("master")
         if use_part_table:
-            src_data["part"] = [dict(prim_attr=e["prim_attr"], sec_attr=i) for i, e in enumerate(src_data["master"])]
-        if use_external:
-            add_ext_files("master")
-            if use_part_table:
-                add_ext_files("part")
-        return src_data
-
-    return _get_src_data
+            add_ext_files("part")
+    return src_data
 
 
 @pytest.fixture
-def get_exp_local_data(get_src_data, src_db_config, local_temp_dir):
-    def _get_exp_local_data(use_part_table, use_external):
-        def create_exp_local_data(kind):
-            exp_local_data[kind] = [
-                dict(e, remote_host=src_db_config.name, remote_schema=src_db_config.schema_name) for e in src_data[kind]
-            ]
+def exp_local_data(use_part_table, use_external, src_data, src_db_config, local_temp_dir):
+    def create_exp_local_data(kind):
+        exp_local_data[kind] = [
+            dict(e, remote_host=src_db_config.name, remote_schema=src_db_config.schema_name) for e in src_data[kind]
+        ]
 
-        def convert_ext_paths(kind):
-            for entity in exp_local_data[kind]:
-                entity["ext_attr"] = os.path.join(local_temp_dir, os.path.basename(entity["ext_attr"]))
+    def convert_ext_paths(kind):
+        for entity in exp_local_data[kind]:
+            entity["ext_attr"] = os.path.join(local_temp_dir, os.path.basename(entity["ext_attr"]))
 
-        src_data = get_src_data(use_part_table=use_part_table, use_external=use_external)
-        exp_local_data = dict()
-        create_exp_local_data("master")
+    exp_local_data = dict()
+    create_exp_local_data("master")
+    if use_part_table:
+        create_exp_local_data("part")
+    if use_external:
+        convert_ext_paths("master")
         if use_part_table:
-            create_exp_local_data("part")
-        if use_external:
-            convert_ext_paths("master")
-            if use_part_table:
-                convert_ext_paths("part")
-        return exp_local_data
-
-    return _get_exp_local_data
+            convert_ext_paths("part")
+    return exp_local_data
 
 
 @pytest.fixture
-def get_src_table(src_store_config):
-    def _get_src_table(use_part_table, use_external):
-        def add_ext_attr(definition):
-            definition += "ext_attr: attach@" + src_store_config.name
-            return definition
+def src_table_cls(use_part_table, use_external, src_store_config):
+    def add_ext_attr(definition):
+        definition += "ext_attr: attach@" + src_store_config.name
+        return definition
 
-        master_definition = """
-        prim_attr: int
+    master_definition = """
+    prim_attr: int
+    ---
+    sec_attr: int
+    """
+    if use_external:
+        master_definition = add_ext_attr(master_definition)
+
+    class Table(dj.Manual):
+        definition = master_definition
+
+    if use_part_table:
+        part_definition = """
+        -> master
         ---
         sec_attr: int
         """
         if use_external:
-            master_definition = add_ext_attr(master_definition)
+            part_definition = add_ext_attr(part_definition)
 
-        class Table(dj.Manual):
-            definition = master_definition
+        class Part(dj.Part):
+            definition = part_definition
 
-        if use_part_table:
-            part_definition = """
-            -> master
-            ---
-            sec_attr: int
-            """
-            if use_external:
-                part_definition = add_ext_attr(part_definition)
+        Table.Part = Part
 
-            class Part(dj.Part):
-                definition = part_definition
-
-            Table.Part = Part
-
-        return Table
-
-    return _get_src_table
+    return Table
 
 
 @pytest.fixture
-def get_src_table_with_data(src_schema, get_src_table, get_src_data):
-    def _get_src_table_with_data(use_part_table, use_external):
-        src_table = get_src_table(use_part_table=use_part_table, use_external=use_external)
-        src_table = src_schema(src_table)
-        src_data = get_src_data(use_part_table=use_part_table, use_external=use_external)
-        src_table.insert(src_data["master"])
-        if use_part_table:
-            src_table.Part().insert(src_data["part"])
-        return src_table
-
-    return _get_src_table_with_data
+def src_table_with_data(use_part_table, src_schema, src_table_cls, src_data):
+    src_table = src_schema(src_table_cls)
+    src_table.insert(src_data["master"])
+    if use_part_table:
+        src_table.Part().insert(src_data["part"])
+    return src_table
 
 
 @pytest.fixture
-def get_local_data(local_schema, src_db_config, get_src_table_with_data, local_temp_dir):
-    def _get_local_data(use_part_table, use_external):
-        get_src_table_with_data(use_part_table=use_part_table, use_external=use_external)
+def local_data(use_part_table, local_schema, src_db_config, src_table_with_data, local_temp_dir):
+    os.environ["REMOTE_DJ_USER"] = src_db_config.users["dj_user"].name
+    os.environ["REMOTE_DJ_PASS"] = src_db_config.users["dj_user"].password
+    remote_schema = main.SchemaProxy(src_db_config.schema_name, host=src_db_config.name)
 
-        os.environ["REMOTE_DJ_USER"] = src_db_config.users["dj_user"].name
-        os.environ["REMOTE_DJ_PASS"] = src_db_config.users["dj_user"].password
-        remote_schema = main.SchemaProxy(src_db_config.schema_name, host=src_db_config.name)
+    @main.Link(local_schema, remote_schema)
+    class Table:
+        pass
 
-        @main.Link(local_schema, remote_schema)
-        class Table:
-            pass
-
-        Table().pull()
-        local_data = dict(master=Table().fetch(as_dict=True, download_path=local_temp_dir))
-        if use_part_table:
-            local_data["part"] = Table.Part().fetch(as_dict=True, download_path=local_temp_dir)
-        return local_data
-
-    return _get_local_data
+    Table().pull()
+    local_data = dict(master=Table().fetch(as_dict=True, download_path=local_temp_dir))
+    if use_part_table:
+        local_data["part"] = Table.Part().fetch(as_dict=True, download_path=local_temp_dir)
+    return local_data
 
 
-def test_pull(get_local_data, get_exp_local_data):
-    assert get_local_data(use_part_table=False, use_external=False) == get_exp_local_data(
-        use_part_table=False, use_external=False
-    )
+@pytest.fixture(params=(True, False), ids=["with_part_table", "without_part_table"])
+def use_part_table(request):
+    return request.param
 
 
-def test_pull_with_part_table(get_local_data, get_exp_local_data):
-    assert get_local_data(use_part_table=True, use_external=False) == get_exp_local_data(
-        use_part_table=True, use_external=False
-    )
+@pytest.fixture(params=(True, False), ids=["with_external", "without_external"])
+def use_external(request):
+    return request.param
 
 
-def test_pull_with_external_files(get_local_data, get_exp_local_data):
-    assert get_local_data(use_part_table=False, use_external=True) == get_exp_local_data(
-        use_part_table=False, use_external=True
-    )
-
-
-def test_pull_with_external_files_and_part_table(get_local_data, get_exp_local_data):
-    assert get_local_data(use_part_table=True, use_external=True) == get_exp_local_data(
-        use_part_table=True, use_external=True
-    )
+def test_pull(local_data, exp_local_data):
+    assert local_data == exp_local_data
