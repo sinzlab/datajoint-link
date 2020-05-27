f61987f2ef606997d6b788e7f0c309c3dc82893e
christoph <chris24.blessing@gmail.com>
2020-05-27 11:22:26 +0200

    Test pulling in a less complex way

---
+++ tests/functional/test_pulling/test_pulling.py
@@ -0,0 +1,62 @@
+import os
+
+import pytest
+import datajoint as dj
+
+from link import main
+
+
+@pytest.fixture
+def src_table_cls(src_store_config):
+    class Table(dj.Manual):
+        definition = """
+        prim_attr: int
+        ---
+        sec_attr: int
+        """
+
+    return Table
+
+
+@pytest.fixture
+def src_data():
+    return [dict(prim_attr=i, sec_attr=-i) for i in range(10)]
+
+
+@pytest.fixture
+def src_table_with_data(src_schema, src_table_cls, src_data):
+    src_table = src_schema(src_table_cls)
+    src_table.insert(src_data)
+    return src_table
+
+
+@pytest.fixture
+def remote_schema(src_db_config):
+    os.environ["REMOTE_DJ_USER"] = src_db_config.users["dj_user"].name
+    os.environ["REMOTE_DJ_PASS"] = src_db_config.users["dj_user"].password
+    return main.SchemaProxy(src_db_config.schema_name, host=src_db_config.name)
+
+
+@pytest.fixture
+def local_table_cls(local_schema, remote_schema):
+    @main.Link(local_schema, remote_schema)
+    class Table:
+        """Local table."""
+
+    return Table
+
+
+@pytest.fixture
+def pulled_data(local_table_cls):
+    local_table_cls().pull()
+    return local_table_cls().fetch(as_dict=True)
+
+
+@pytest.fixture
+def expected_pulled_data(src_data, src_db_config):
+    return [dict(e, remote_host=src_db_config.name, remote_schema=src_db_config.schema_name) for e in src_data]
+
+
+@pytest.mark.usefixtures("src_table_with_data")
+def test_pulling(pulled_data, expected_pulled_data):
+    assert pulled_data == expected_pulled_data
