d2c7f188c12589fe036f987578c9a8a8e9afcfaf
christoph <chris24.blessing@gmail.com>
2020-06-18 19:58:30 +0200

    Tie whether repository is in transaction or not to whether or not entities are backed up

--- link/entities/repository.py
+++ link/entities/repository.py
@@ -6,7 +6,6 @@ class Repository:
         """Initializes Repository."""
         self.address = address
         self._entities = self._create_entities(self.gateway.get_identifiers())
-        self._in_transaction = False
         self._backup = None
 
     def _create_entities(self, identifiers):
@@ -40,27 +39,26 @@ class Repository:
 
     @property
     def in_transaction(self):
-        return self._in_transaction
+        return bool(self._backup)
 
     def start_transaction(self):
         if self.in_transaction:
             raise RuntimeError("Can't start transaction while in transaction")
         self.gateway.start_transaction()
         self._backup = self._create_entities(self._entities)
-        self._in_transaction = True
 
     def commit_transaction(self):
         if not self.in_transaction:
             raise RuntimeError("Can't commit transaction while not in transaction")
         self.gateway.commit_transaction()
-        self._in_transaction = False
+        self._backup = None
 
     def cancel_transaction(self):
         if not self.in_transaction:
             raise RuntimeError("Can't cancel transaction while not in transaction")
         self.gateway.cancel_transaction()
         self._entities = self._create_entities(self._backup)
-        self._in_transaction = False
+        self._backup = None
 
     def __contains__(self, identifier):
         return identifier in self._entities
