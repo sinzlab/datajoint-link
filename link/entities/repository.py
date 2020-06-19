8971a013815bb8f4c328b88414af085e2cd01607
christoph <chris24.blessing@gmail.com>
2020-06-19 14:22:15 +0200

    Add type information to signatures

--- link/entities/repository.py
+++ link/entities/repository.py
@@ -1,5 +1,9 @@
+from typing import List, Dict
 from functools import wraps
 from contextlib import contextmanager
+
+from .address import Address
+from .entity import Entity
 
 
 def _check_identifiers(function):
@@ -17,13 +21,13 @@ class Repository:
     gateway = None
     entity_cls = None
 
-    def __init__(self, address):
+    def __init__(self, address: Address) -> None:
         """Initializes Repository."""
         self.address = address
         self._entities = self._create_entities(self.gateway.get_identifiers())
         self._backup = None
 
-    def _create_entities(self, identifiers):
+    def _create_entities(self, identifiers: List[str]) -> Dict[str, Entity]:
         return {i: self.entity_cls(self.address, i) for i in identifiers}
 
     @property
@@ -31,22 +35,22 @@ class Repository:
         return list(self._entities)
 
     @property
-    def entities(self):
+    def entities(self) -> List[Entity]:
         return list(self._entities.values())
 
     @_check_identifiers
-    def fetch(self, identifiers):
+    def fetch(self, identifiers: List[str]) -> List[Entity]:
         self.gateway.fetch(identifiers)
         entities = [self._entities[i] for i in identifiers]
         return entities
 
     @_check_identifiers
-    def delete(self, identifiers):
+    def delete(self, identifiers: List[str]) -> None:
         self.gateway.delete(identifiers)
         for identifier in identifiers:
             del self._entities[identifier]
 
-    def insert(self, entities):
+    def insert(self, entities: List[Entity]) -> None:
         for entity in entities:
             if entity.address != self.address:
                 raise ValueError(
@@ -59,24 +63,24 @@ class Repository:
             self._entities[entity.identifier] = entity
 
     @property
-    def in_transaction(self):
+    def in_transaction(self) -> bool:
         if self._backup is None:
             return False
         return True
 
-    def start_transaction(self):
+    def start_transaction(self) -> None:
         if self.in_transaction:
             raise RuntimeError("Can't start transaction while in transaction")
         self.gateway.start_transaction()
         self._backup = self._create_entities(self.identifiers)
 
-    def commit_transaction(self):
+    def commit_transaction(self) -> None:
         if not self.in_transaction:
             raise RuntimeError("Can't commit transaction while not in transaction")
         self.gateway.commit_transaction()
         self._backup = None
 
-    def cancel_transaction(self):
+    def cancel_transaction(self) -> None:
         if not self.in_transaction:
             raise RuntimeError("Can't cancel transaction while not in transaction")
         self.gateway.cancel_transaction()
@@ -94,5 +98,5 @@ class Repository:
         else:
             self.commit_transaction()
 
-    def __contains__(self, identifier):
+    def __contains__(self, identifier) -> bool:
         return identifier in self.identifiers
