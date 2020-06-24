59f33c61cb5ddc65be088a04f32ac5c795108537
christoph <chris24.blessing@gmail.com>
2020-06-24 11:25:45 +0200

    Remove source and move back fixtures

--- tests/unit/entities/test_repository.py
+++ tests/unit/entities/test_repository.py
@@ -14,6 +14,31 @@ def test_if_entity_cls_is_none_by_default():
 
 
 @pytest.fixture
+def address_cls():
+    class Address:
+        def __init__(self, name):
+            self.name = name
+
+        def __eq__(self, other):
+            return self.name == other.name
+
+        def __repr__(self):
+            return self.name
+
+    return Address
+
+
+@pytest.fixture
+def address(address_cls):
+    return address_cls("address")
+
+
+@pytest.fixture
+def identifiers():
+    return ["ID" + str(i) for i in range(10)]
+
+
+@pytest.fixture
 def gateway(identifiers):
     gateway = MagicMock(name="gateway")
     gateway.get_identifiers.return_value = identifiers.copy()
