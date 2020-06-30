3f42ddc849d2b940da23b8ee470acbddc1e04ee7
christoph <chris24.blessing@gmail.com>
2020-06-30 12:59:46 +0200

    Strip out unnecessary code

--- link/use_cases/base.py
+++ link/use_cases/base.py
@@ -3,8 +3,8 @@ from abc import ABC, abstractmethod
 from typing import Callable, Any
 
 
-class InitializationUseCase(ABC):
-    """ABC for use-cases that deal with initialization."""
+class UseCase(ABC):
+    """Specifies the interface for use-cases."""
 
     def __init__(self, output_port: Callable[[Any], None]) -> None:
         """Initializes the use-case."""
@@ -16,33 +16,8 @@ class InitializationUseCase(ABC):
         self.output_port(output)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}({repr(self.output_port)})"
+        return f"{self.__class__.__qualname__}({self.output_port})"
 
     @abstractmethod
     def execute(self, *args, **kwargs):
         """Executes the use-case."""
-
-
-class UseCase(InitializationUseCase):
-    """ABC for all use-cases invoked by the end user."""
-
-    initialize_local: InitializationUseCase = None
-    initialize_source: InitializationUseCase = None
-
-    @property
-    @abstractmethod
-    def requires_local(self) -> bool:
-        """Whether the execution of the use-case requires a connection to the local side."""
-
-    @property
-    @abstractmethod
-    def requires_source(self) -> bool:
-        """Whether the execution of the use-case requires a connection to the source side."""
-
-    def __call__(self, *args, **kwargs) -> None:
-        """Initializes the local and/or source side(s) if required and then executes the use-case."""
-        if self.requires_local:
-            self.initialize_local()
-        if self.requires_source:
-            self.initialize_source()
-        super().__call__(*args, **kwargs)
--- tests/unit/use_cases/test_base.py
+++ tests/unit/use_cases/test_base.py
@@ -7,8 +7,9 @@ from link.use_cases import base
 
 @pytest.fixture
 def output_port():
-    output_port = MagicMock(name="output_port")
-    output_port.__repr__ = MagicMock(name="output_port.__repr__", return_value="output_port")
+    name = "output_port"
+    output_port = MagicMock(name=name)
+    output_port.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
     return output_port
 
 
@@ -20,7 +21,7 @@ def execute_call():
 class TestInitializationUseCase:
     @pytest.fixture
     def use_case(self, output_port):
-        class UseCase(base.InitializationUseCase):
+        class UseCase(base.UseCase):
             execute = MagicMock(name="execute", return_value="executed!")
             __qualname__ = "UseCase"
 
@@ -39,57 +40,3 @@ class TestInitializationUseCase:
 
     def test_repr(self, use_case):
         assert repr(use_case) == "UseCase(output_port)"
-
-
-class TestUseCase:
-    @pytest.fixture
-    def use_case_cls(self):
-        class UseCase(base.UseCase):
-            initialize_local = MagicMock(name="initialize_local")
-            initialize_source = MagicMock(name="initialize_source")
-            execute = MagicMock(name="execute")
-            requires_local = requires_source = False
-
-        return UseCase
-
-    @pytest.fixture
-    def use_case(self, use_case_cls, output_port):
-        return use_case_cls(output_port)
-
-    def test_if_initialize_local_is_none_by_default(self):
-        assert base.UseCase.initialize_local is None
-
-    def test_if_initialize_source_is_none_by_default(self):
-        assert base.UseCase.initialize_source is None
-
-    def test_if_local_side_is_initialized_if_required_when_calling_use_case(self, use_case):
-        use_case.requires_local = True
-        use_case()
-        use_case.initialize_local.assert_called_once_with()
-        use_case.initialize_source.assert_not_called()
-
-    def test_if_source_side_is_initialized_if_required_when_calling_use_case(self, use_case):
-        use_case.requires_source = True
-        use_case()
-        use_case.initialize_source.assert_called_once_with()
-        use_case.initialize_local.assert_not_called()
-
-    def test_if_both_sides_are_initialized_if_required_when_calling_use_case(self, use_case):
-        use_case.requires_local = True
-        use_case.requires_source = True
-        use_case()
-        use_case.initialize_local.assert_called_once_with()
-        use_case.initialize_source.assert_called_once_with()
-
-    def test_if_execute_method_of_super_class_is_correctly_called_when_calling_use_case(
-        self, use_case_cls, output_port, execute_call
-    ):
-        class FakeInitializationUseCase(base.InitializationUseCase):
-            execute = MagicMock(name="execute")
-            requires_local = requires_source = False
-
-        class UseCase(base.UseCase, FakeInitializationUseCase):
-            requires_local = requires_source = None
-
-        UseCase(output_port)(*execute_call.args, **execute_call.kwargs)
-        FakeInitializationUseCase.execute.assert_called_once_with(*execute_call.args, **execute_call.kwargs)
