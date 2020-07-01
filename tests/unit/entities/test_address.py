7610817ab2afaa413d369fdafc388c26d2349263
christoph <chris24.blessing@gmail.com>
2020-07-01 14:43:47 +0200

    Implement configuration

---
+++ tests/unit/entities/test_address.py
@@ -0,0 +1,129 @@
+import pytest
+
+from link.entities import address
+from link.entities.domain import Address
+
+
+@pytest.fixture
+def table_name():
+    return "table"
+
+
+@pytest.fixture
+def local_host_name():
+    return "local_host"
+
+
+@pytest.fixture
+def local_database_name():
+    return "local_database"
+
+
+@pytest.fixture
+def source_host_name():
+    return "source_host"
+
+
+@pytest.fixture
+def source_database_name():
+    return "source_database"
+
+
+@pytest.fixture
+def outbound_database_name():
+    return "outbound_database"
+
+
+@pytest.fixture
+def outbound_table_name():
+    return "outbound_table"
+
+
+@pytest.fixture
+def info(
+    table_name,
+    local_host_name,
+    local_database_name,
+    source_host_name,
+    source_database_name,
+    outbound_database_name,
+    outbound_table_name,
+):
+    return [
+        table_name,
+        local_host_name,
+        local_database_name,
+        source_host_name,
+        source_database_name,
+        outbound_database_name,
+        outbound_table_name,
+    ]
+
+
+@pytest.fixture
+def config():
+    return address.Configuration()
+
+
+@pytest.fixture
+def configure(config, info):
+    def _configure():
+        config.configure(*info)
+
+    _configure()
+    return _configure
+
+
+def test_if_is_configured_flag_is_false_if_not_configured(config):
+    assert config.is_configured is False
+
+
+class TestConfigure:
+    @pytest.mark.usefixtures("configure")
+    def test_if_is_configured_flag_is_switched(self, config):
+        assert config.is_configured is True
+
+    def test_if_runtime_error_is_raised_if_already_configured(self, config, configure):
+        with pytest.raises(RuntimeError):
+            configure()
+
+
+class TestLocalAddressProperty:
+    def test_if_runtime_error_is_raised_if_not_configured(self, config):
+        with pytest.raises(RuntimeError):
+            _ = config.local_address
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_correct_address_is_returned(self, config, table_name, local_host_name, local_database_name):
+        assert config.local_address == Address(local_host_name, local_database_name, table_name)
+
+
+class TestSourceAddressProperty:
+    def test_if_runtime_error_is_raised_if_not_configured(self, config):
+        with pytest.raises(RuntimeError):
+            _ = config.source_address
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_correct_address_is_returned(self, config, table_name, source_host_name, source_database_name):
+        assert config.source_address == Address(source_host_name, source_database_name, table_name)
+
+
+class TestOutboundAddressProperty:
+    def test_if_runtime_error_is_raised_not_configured(self, config):
+        with pytest.raises(RuntimeError):
+            _ = config.outbound_address
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_correct_address_is_returned(
+        self, config, outbound_table_name, source_host_name, outbound_database_name
+    ):
+        assert config.outbound_address == Address(source_host_name, outbound_database_name, outbound_table_name)
+
+
+class TestRepr:
+    def test_while_not_configured(self, config):
+        assert repr(config) == "Configuration()"
+
+    @pytest.mark.usefixtures("configure")
+    def test_while_configured(self, config, info):
+        assert repr(config) == f"Configuration().configure(" + ", ".join(info) + ")"
