90a2010abea98b58375782cb8d55ee2df32e155a
christoph <chris24.blessing@gmail.com>
2020-07-03 13:10:01 +0200

    Implement datajoint gateway

---
+++ link/adapters/dj_gateway.py
@@ -0,0 +1,80 @@
+import json
+import hashlib
+from typing import List, Dict, Any
+from functools import wraps
+
+from . import gateway
+
+
+def _identifiers_to_primary_keys(method):
+    @wraps(method)
+    def wrapper(self, identifiers):
+        primary_keys = [self._identifiers_to_primary_keys_mapping[identifier] for identifier in identifiers]
+        return method(self, primary_keys)
+
+    return wrapper
+
+
+class ReadOnlyGateway(gateway.AbstractReadOnlyGateway):
+    def __init__(self, table):
+        self.table = table
+
+    @property
+    def identifiers(self) -> List[str]:
+        return [self._hash_primary_key(key) for key in self.table.primary_keys]
+
+    @_identifiers_to_primary_keys
+    def fetch(self, identifiers: List[str]) -> Dict[str, Any]:
+        data = dict()
+        for entity in self.table.fetch(identifiers):
+            primary_key = {pan: entity.pop(pan) for pan in self.table.primary_attr_names}
+            data[self._hash_primary_key(primary_key)] = entity
+        return data
+
+    @property
+    def _identifiers_to_primary_keys_mapping(self):
+        return {self._hash_primary_key(key): key for key in self.table.primary_keys}
+
+    @staticmethod
+    def _hash_primary_key(primary_key):
+        return hashlib.sha1(json.dumps(primary_key, sort_keys=True).encode()).hexdigest()
+
+
+class Gateway(ReadOnlyGateway, gateway.AbstractGateway):
+    @property
+    def deletion_requested_identifiers(self) -> List[str]:
+        return [self._hash_primary_key(key) for key in self.table.deletion_requested]
+
+    @property
+    def deletion_approved_identifiers(self) -> List[str]:
+        return [self._hash_primary_key(key) for key in self.table.deletion_approved]
+
+    @_identifiers_to_primary_keys
+    def delete(self, identifiers: List[str]) -> None:
+        self.table.delete(identifiers)
+
+    @_identifiers_to_primary_keys
+    def insert(self, identifiers: List[str]) -> None:
+        self.table.insert(identifiers)
+
+    def start_transaction(self) -> None:
+        self.table.start_transaction()
+
+    def commit_transaction(self) -> None:
+        self.table.commit_transaction()
+
+    def cancel_transaction(self) -> None:
+        self.table.cancel_transaction()
+
+
+class SourceGateway(ReadOnlyGateway, gateway.AbstractSourceGateway):
+    pass
+
+
+class OutboundGateway(Gateway, gateway.AbstractOutboundGateway):
+    def approve_deletion(self, identifiers: List[str]) -> None:
+        pass
+
+
+class LocalGateway(Gateway, gateway.AbstractLocalGateway):
+    pass
--- tests/unit/adapters/test_dj_gateway.py
+++ tests/unit/adapters/test_dj_gateway.py
@@ -7,13 +7,65 @@ from link.adapters import gateway as abstract_gateway
 
 
 @pytest.fixture
-def primary_keys():
-    return [dict(a=1, b=2.5, c="Hello world!"), dict(a=5, b=5.12, c="Goodbye world!"), dict(a=12, b=14.64, c="Yo!")]
+def primary_attr_names():
+    return ["p0", "p1", "p2"]
 
 
 @pytest.fixture
-def table(primary_keys):
-    return MagicMock(name="table", primary_keys=primary_keys)
+def primary_attr_data():
+    return [(1, 2.5, "Hello world!"), (5, 5.12, "Goodbye world!"), (12, 14.64, "Yo!")]
+
+
+@pytest.fixture
+def secondary_attr_names():
+    return ["s0", "s1"]
+
+
+@pytest.fixture
+def secondary_attr_data():
+    return [(4.5, 12), (7.51, 64), (5.123, 34)]
+
+
+@pytest.fixture
+def primary_keys(primary_attr_names, primary_attr_data):
+    primary_keys = []
+    for data in primary_attr_data:
+        primary_keys.append({k: v for k, v in zip(primary_attr_names, data)})
+    return primary_keys
+
+
+@pytest.fixture
+def entities(primary_keys, secondary_attr_names, secondary_attr_data):
+    entities = []
+    for primary_key, data in zip(primary_keys, secondary_attr_data):
+        entity = {k: v for k, v in zip(secondary_attr_names, data)}
+        entity.update(primary_key)
+        entities.append(entity)
+    return entities
+
+
+@pytest.fixture
+def table(primary_attr_names, primary_keys, entities):
+    table = MagicMock(name="table", primary_attr_names=primary_attr_names, primary_keys=primary_keys)
+    table.fetch.return_value = entities
+    return table
+
+
+@pytest.fixture
+def identifiers():
+    return [
+        "edbaaf579279b704bd776d22a35b47a7477702de",
+        "25d0f885e46404fa3774b32f614e086c4afa7f4f",
+        "c8a5fac2306aab1cc2d12459c575bde80c403a02",
+    ]
+
+
+@pytest.fixture
+def data(identifiers, secondary_attr_names, secondary_attr_data):
+    return {
+        identifier: {k: v for k, v in zip(secondary_attr_names, data)}
+        for identifier, data in zip(identifiers, secondary_attr_data)
+    }
 
 
 @pytest.fixture
@@ -21,22 +73,13 @@ def gateway(gateway_cls, table):
     return gateway_cls(table)
 
 
-@pytest.fixture
-def identifiers():
-    return [
-        "53eece2e8abc80f75d06f0e08da5da9a88210116",
-        "9fde7aa1c88f25abbbc39d184352acf451dff348",
-        "e41a74018879ec8fe0dc0d402a58234a9cfc9789",
-    ]
-
-
-class TestGateway:
-    def test_if_subclass_of_abstract_gateway(self):
-        assert issubclass(dj_gateway.Gateway, abstract_gateway.AbstractGateway)
+class TestReadOnlyGateway:
+    def test_if_subclass_of_abstract_read_only_gateway(self):
+        assert issubclass(dj_gateway.ReadOnlyGateway, abstract_gateway.AbstractReadOnlyGateway)
 
     @pytest.fixture
     def gateway_cls(self):
-        class Gateway(dj_gateway.Gateway):
+        class Gateway(dj_gateway.ReadOnlyGateway):
             pass
 
         return Gateway
@@ -47,9 +90,39 @@ class TestGateway:
     def test_identifiers_property(self, gateway, identifiers):
         assert gateway.identifiers == identifiers
 
-    def test_fetch(self, primary_keys, table, gateway, identifiers):
+    def test_if_data_is_fetched_from_table(self, primary_keys, table, gateway, identifiers):
         gateway.fetch(identifiers)
         table.fetch.assert_called_once_with(primary_keys)
+
+    def test_if_data_is_returned(self, primary_keys, gateway, identifiers, data):
+        assert gateway.fetch(identifiers) == data
+
+
+class TestGateway:
+    def test_if_subclass_of_read_only_gateway(self):
+        assert issubclass(dj_gateway.Gateway, dj_gateway.ReadOnlyGateway)
+
+    def test_if_subclass_of_abstract_gateway(self):
+        assert issubclass(dj_gateway.Gateway, abstract_gateway.AbstractGateway)
+
+    @pytest.fixture
+    def gateway_cls(self):
+        class FlaggedGateway(dj_gateway.Gateway):
+            pass
+
+        return FlaggedGateway
+
+    @pytest.fixture
+    def table(self, table, primary_keys):
+        table.deletion_requested = [primary_keys[0], primary_keys[1]]
+        table.deletion_approved = [primary_keys[0]]
+        return table
+
+    def test_deletion_requested_identifiers(self, gateway, identifiers):
+        assert gateway.deletion_requested_identifiers == [identifiers[0], identifiers[1]]
+
+    def test_deletion_approved_identifiers(self, gateway, identifiers):
+        assert gateway.deletion_approved_identifiers == [identifiers[0]]
 
     def test_delete(self, primary_keys, table, gateway, identifiers):
         gateway.delete(identifiers)
@@ -72,52 +145,25 @@ class TestGateway:
         table.cancel_transaction.assert_called_once_with()
 
 
-class TestFlaggedGateway:
-    def test_if_subclass_of_gateway(self):
-        assert issubclass(dj_gateway.FlaggedGateway, dj_gateway.Gateway)
-
-    def test_if_subclass_of_abstract_flagged_gateway(self):
-        assert issubclass(dj_gateway.FlaggedGateway, abstract_gateway.AbstractFlaggedGateway)
-
-    @pytest.fixture
-    def gateway_cls(self):
-        class FlaggedGateway(dj_gateway.FlaggedGateway):
-            pass
-
-        return FlaggedGateway
-
-    @pytest.fixture
-    def table(self, table, primary_keys):
-        table.deletion_requested = [primary_keys[0], primary_keys[1]]
-        table.deletion_approved = [primary_keys[0]]
-        return table
-
-    def test_deletion_requested_flags(self, gateway, identifiers):
-        assert gateway.deletion_requested_flags == [identifiers[0], identifiers[1]]
-
-    def test_deletion_approved_flags(self, gateway, identifiers):
-        assert gateway.deletion_approved_flags == [identifiers[0]]
-
-
 class TestSourceGateway:
-    def test_if_subclass_of_flagged_gateway(self):
-        assert issubclass(dj_gateway.SourceGateway, dj_gateway.FlaggedGateway)
+    def test_if_subclass_of_read_only_gateway(self):
+        assert issubclass(dj_gateway.SourceGateway, dj_gateway.ReadOnlyGateway)
 
     def test_if_subclass_of_abstract_source_gateway(self):
         assert issubclass(dj_gateway.SourceGateway, abstract_gateway.AbstractSourceGateway)
 
 
 class TestOutboundGateway:
-    def test_if_subclass_of_flagged_gateway(self):
-        assert issubclass(dj_gateway.OutboundGateway, dj_gateway.FlaggedGateway)
+    def test_if_subclass_of_gateway(self):
+        assert issubclass(dj_gateway.OutboundGateway, dj_gateway.Gateway)
 
     def test_if_subclass_of_abstract_outbound_gateway(self):
         assert issubclass(dj_gateway.OutboundGateway, abstract_gateway.AbstractOutboundGateway)
 
 
 class TestLocalGateway:
-    def test_if_subclass_of_flagged_gateway(self):
-        assert issubclass(dj_gateway.LocalGateway, dj_gateway.FlaggedGateway)
+    def test_if_subclass_of_gateway(self):
+        assert issubclass(dj_gateway.LocalGateway, dj_gateway.Gateway)
 
     def test_if_subclass_of_abstract_local_gateway(self):
         assert issubclass(dj_gateway.LocalGateway, abstract_gateway.AbstractLocalGateway)
