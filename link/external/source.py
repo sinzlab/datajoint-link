7bdee31d688c09c5fd1a310ae01c978737fb2d10
christoph <chris24.blessing@gmail.com>
2020-07-08 17:51:48 +0200

    Implement outbound table factory

--- link/external/source.py
+++ link/external/source.py
@@ -14,3 +14,19 @@ class SourceTableFactory:
 
     def __repr__(self) -> str:
         return self.__class__.__qualname__ + "()"
+
+
+class OutboundTableFactory(SourceTableFactory):
+    def __init__(self, source_table_cls, table_cls):
+        super().__init__()
+        self.source_table_cls = source_table_cls
+        self.table_cls = table_cls
+
+    def __call__(self):
+        self.table_cls.source_table_cls = self.source_table_cls
+        self.table_cls.__name__ = self.table_name + "Outbound"
+        self.schema(self.table_cls)
+        return self.table_cls()
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}({self.source_table_cls}, {self.table_cls})"
--- tests/unit/external/test_source.py
+++ tests/unit/external/test_source.py
@@ -7,16 +7,13 @@ from link.external import source
 
 
 @pytest.fixture
-def factory():
-    return source.SourceTableFactory()
+def factory_args():
+    return list()
 
 
-def test_if_schema_is_none(factory):
-    assert factory.schema is None
-
-
-def test_if_table_name_is_none(factory):
-    assert factory.table_name is None
+@pytest.fixture
+def factory(factory_cls, factory_args):
+    return factory_cls(*factory_args)
 
 
 @pytest.fixture
@@ -25,31 +22,38 @@ def table_name():
 
 
 @pytest.fixture
-def source_table():
-    return MagicMock(name="source_table")
+def table(factory_type):
+    return MagicMock(name=factory_type + "_table")
 
 
 @pytest.fixture
-def source_table_cls(source_table):
-    return MagicMock(name="source_table_cls", return_value=source_table)
+def table_cls(factory_type, table):
+    name = factory_type + "_table_cls"
+    table_cls = MagicMock(name=name, return_value=table)
+    table_cls.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+    return table_cls
 
 
 @pytest.fixture
-def source_schema(table_name, source_table_cls):
-    class SourceSchema:
+def schema(factory_type, table_name, table_cls):
+    class Schema:
         @staticmethod
         def spawn_missing_classes(context=None):
-            context[table_name] = source_table_cls
+            context[table_name] = table_cls
 
-    SourceSchema.spawn_missing_classes = MagicMock(
-        name="SourceSchema.spawn_missing_classes", wraps=SourceSchema.spawn_missing_classes
+        def __call__(self, _):
+            return table_cls
+
+    Schema.__name__ = factory_type.title() + Schema.__name__
+    Schema.spawn_missing_classes = MagicMock(
+        name=factory_type.title() + "Schema.spawn_missing_classes", wraps=Schema.spawn_missing_classes
     )
-    return SourceSchema()
+    return MagicMock(name=Schema.__name__, wraps=Schema())
 
 
 @pytest.fixture
-def configure(factory, source_schema, table_name):
-    factory.schema = source_schema
+def configure(factory, schema, table_name):
+    factory.schema = schema
     factory.table_name = table_name
 
 
@@ -70,23 +74,90 @@ def copy_call_args():
     return _copy_call_args
 
 
-@pytest.mark.usefixtures("configure")
-def test_if_missing_classes_are_spawned(factory, source_schema, copy_call_args):
-    new_mock = copy_call_args(source_schema.spawn_missing_classes)
-    factory()
-    new_mock.assert_called_once_with(context=dict())
+class TestSourceTableFactory:
+    @pytest.fixture
+    def factory_type(self):
+        return "source"
+
+    @pytest.fixture
+    def factory_cls(self):
+        return source.SourceTableFactory
+
+    def test_if_schema_is_none(self, factory):
+        assert factory.schema is None
+
+    def test_if_table_name_is_none(self, factory):
+        assert factory.table_name is None
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_missing_classes_are_spawned(self, factory, schema, copy_call_args):
+        new_mock = copy_call_args(schema.spawn_missing_classes)
+        factory()
+        new_mock.assert_called_once_with(context=dict())
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_source_table_cls_is_instantiated(self, factory, table_cls):
+        factory()
+        table_cls.assert_called_once_with()
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_source_table_is_returned(self, factory, table):
+        assert factory() == table
+
+    def test_repr(self, factory):
+        assert repr(factory) == "SourceTableFactory()"
 
 
-@pytest.mark.usefixtures("configure")
-def test_if_source_table_cls_is_instantiated(factory, source_table_cls):
-    factory()
-    source_table_cls.assert_called_once_with()
+class TestOutboundTableFactory:
+    @pytest.fixture
+    def factory_type(self):
+        return "outbound"
 
+    @pytest.fixture
+    def factory_cls(self):
+        return source.OutboundTableFactory
 
-@pytest.mark.usefixtures("configure")
-def test_if_source_table_is_returned(factory, source_table):
-    assert factory() == source_table
+    @pytest.fixture
+    def source_table_factory(self):
+        name = "source_table_factory"
+        source_table_factory = MagicMock(name=name)
+        source_table_factory.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+        return source_table_factory
 
+    @pytest.fixture
+    def factory_args(self, source_table_factory, table_cls):
+        return [source_table_factory, table_cls]
 
-def test_repr(factory):
-    assert repr(factory) == "SourceTableFactory()"
+    def test_if_subclass_of_source_table_factory(self, factory_cls):
+        assert issubclass(factory_cls, source.SourceTableFactory)
+
+    def test_if_table_cls_is_stored_as_instance_attribute(self, factory, table_cls):
+        assert factory.table_cls is table_cls
+
+    def test_if_source_table_is_stored_as_instance_attribute(self, factory, source_table_factory):
+        assert factory.source_table_cls is source_table_factory
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_source_table_cls_attribute_of_outbound_table_cls_is_correctly_set(
+        self, factory, source_table_factory, table_cls
+    ):
+        factory()
+        assert table_cls.source_table_cls is source_table_factory
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_name_attribute_of_outbound_table_cls_is_correctly_set(self, factory, table_name, table_cls):
+        factory()
+        assert table_cls.__name__ == table_name + "Outbound"
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_outbound_schema_is_applied_to_outbound_table_class(self, factory, schema, table_cls):
+        factory()
+        print(schema)
+        schema.assert_called_once_with(table_cls)
+
+    @pytest.mark.usefixtures("configure")
+    def test_if_outbound_table_is_returned(self, factory, table):
+        assert factory() is table
+
+    def test_repr(self, factory):
+        assert repr(factory) == "OutboundTableFactory(source_table_factory, outbound_table_cls)"
