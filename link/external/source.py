ca81af73447e8c1a113ca71434e88d1770c045a8
christoph <chris24.blessing@gmail.com>
2020-07-09 17:32:20 +0200

    Add reference to part tables to spawned table class as attribute

--- link/external/source.py
+++ link/external/source.py
@@ -1,4 +1,7 @@
 from typing import Type
+from inspect import isclass
+
+from datajoint import Part
 from datajoint.table import Table
 
 
@@ -6,6 +9,7 @@ class SourceTableFactory:
     def __init__(self) -> None:
         self.schema = None
         self.table_name = None
+        self._ignored_parts = []
 
     def __call__(self) -> Table:
         source_table_cls = self.spawn_table_cls()
@@ -14,7 +18,18 @@ class SourceTableFactory:
     def spawn_table_cls(self) -> Type[Table]:
         table_classes = dict()
         self.schema.spawn_missing_classes(context=table_classes)
-        return table_classes[self.table_name]
+        table_cls = table_classes[self.table_name]
+        table_cls.parts = self._get_part_tables(table_cls)
+        return table_cls
+
+    def _get_part_tables(self, table_cls):
+        parts = dict()
+        for name in dir(table_cls):
+            if name[0].isupper() and name not in self._ignored_parts:
+                attr = getattr(table_cls, name)
+                if isclass(attr) and issubclass(attr, Part):
+                    parts[name] = attr
+        return parts
 
     def __repr__(self) -> str:
         return self.__class__.__qualname__ + "()"
--- tests/unit/external/test_external_source.py
+++ tests/unit/external/test_external_source.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock, DEFAULT
 from copy import deepcopy
 
 import pytest
+from datajoint import Part
 
 
 @pytest.fixture
@@ -41,6 +42,45 @@ def test_if_table_name_is_none(factory):
 
 @pytest.mark.usefixtures("configure")
 class TestSpawnTableClass:
+    @pytest.fixture
+    def lowercase_attr(self):
+        return dict(lowercae_attr="lowercase_attr")
+
+    @pytest.fixture
+    def ignored_part_table(self, factory):
+        class IgnoredPartTable(Part):
+            definition = ""
+
+        factory._ignored_parts = [IgnoredPartTable.__name__]
+        return dict(IgnoredPartTable=IgnoredPartTable)
+
+    @pytest.fixture
+    def non_cls_attr(self):
+        return dict(NonClsAttr="NonClsAttr")
+
+    @pytest.fixture
+    def non_part_cls(self):
+        class NotPartTable:
+            pass
+
+        return dict(NotPartTable=NotPartTable)
+
+    @pytest.fixture
+    def part_table(self):
+        class PartTable(Part):
+            definition = ""
+
+        return dict(PartTable=PartTable)
+
+    @pytest.fixture
+    def attrs(self, lowercase_attr, ignored_part_table, non_cls_attr, non_part_cls, part_table):
+        return {**lowercase_attr, **ignored_part_table, **non_cls_attr, **non_part_cls, **part_table}
+
+    @pytest.fixture
+    def add_attrs_to_spawned_table_cls(self, spawned_table_cls, attrs):
+        for name, attr in attrs.items():
+            setattr(spawned_table_cls, name, attr)
+
     def test_if_missing_classes_are_spawned(self, factory, schema, copy_call_args):
         new_mock = copy_call_args(schema.spawn_missing_classes)
         factory.spawn_table_cls()
@@ -48,6 +88,11 @@ class TestSpawnTableClass:
 
     def test_if_table_cls_is_returned(self, factory, spawned_table_cls):
         assert factory.spawn_table_cls() is spawned_table_cls
+
+    @pytest.mark.usefixtures("add_attrs_to_spawned_table_cls")
+    def test_if_part_tables_attribute_is_set_correctly(self, factory, spawned_table_cls, part_table):
+        factory()
+        assert spawned_table_cls.parts == part_table
 
 
 @pytest.mark.usefixtures("configure")
