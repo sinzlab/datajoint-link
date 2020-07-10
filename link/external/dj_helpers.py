e4c4de7cc795acf13925d4a98a00653b0c030a88
christoph <chris24.blessing@gmail.com>
2020-07-10 17:46:00 +0200

    Implement function that returns part table classes of a table class

--- link/external/dj_helpers.py
+++ link/external/dj_helpers.py
@@ -1,5 +1,9 @@
 import re
-from typing import Dict
+from typing import Dict, Type, List, Optional
+from inspect import isclass
+
+from datajoint import Part
+from datajoint.table import Table
 
 
 def replace_stores(definition: str, stores: Dict[str, str]) -> str:
@@ -8,3 +12,15 @@ def replace_stores(definition: str, stores: Dict[str, str]) -> str:
 
     pattern = re.compile(r"(attach@)(\S+)")
     return re.sub(pattern, replace_store, definition)
+
+
+def get_part_table_classes(table_cls: Type[Table], ignored_parts: Optional[List[str]] = None) -> Dict[str, Type[Part]]:
+    if ignored_parts is None:
+        ignored_parts = []
+    part_table_classes = dict()
+    for name in dir(table_cls):
+        if name[0].isupper() and name not in ignored_parts:
+            attr = getattr(table_cls, name)
+            if isclass(attr) and issubclass(attr, Part):
+                part_table_classes[name] = attr
+    return part_table_classes
--- tests/unit/external/test_dj_helpers.py
+++ tests/unit/external/test_dj_helpers.py
@@ -1,61 +1,133 @@
 import pytest
+from datajoint import Part
 
 from link.external import dj_helpers
 
 
-@pytest.fixture
-def original_definition_lines():
-    return ["pa : attach@original_pa_store"]
+class TestReplaceStores:
+    @pytest.fixture
+    def original_definition_lines(self):
+        return ["pa : attach@original_pa_store"]
+
+    @pytest.fixture
+    def original_definition(self, original_definition_lines):
+        return "\n".join(original_definition_lines)
+
+    @pytest.fixture
+    def expected_definition_lines(self):
+        return ["pa : attach@replacement_pa_store"]
+
+    @pytest.fixture
+    def expected_definition(self, expected_definition_lines):
+        return "\n".join(expected_definition_lines)
+
+    @pytest.fixture
+    def stores(self):
+        return dict(original_pa_store="replacement_pa_store")
+
+    @pytest.fixture
+    def add_store(self, original_definition_lines, expected_definition_lines, stores):
+        original_definition_lines.append("pb: attach@original_pb_store")
+        expected_definition_lines.append("pb: attach@replacement_pb_store")
+        stores["original_pb_store"] = "replacement_pb_store"
+
+    @pytest.fixture
+    def add_store_name_outside_of_attached_attribute(
+        self, original_definition_lines, expected_definition_lines, stores
+    ):
+        original_definition_lines.append("pc: int # original_pc_store")
+        expected_definition_lines.append("pc: int # original_pc_store")
+        stores["original_pc_store"] = "replacement_pc_store"
+
+    @pytest.fixture
+    def replacement_matches_expectation(self, original_definition, expected_definition, stores):
+        return dj_helpers.replace_stores(original_definition, stores) == expected_definition
+
+    def test_if_single_store_is_replaced(self, replacement_matches_expectation):
+        assert replacement_matches_expectation
+
+    @pytest.mark.usefixtures("add_store")
+    def test_if_multiple_stores_are_replaced(self, replacement_matches_expectation):
+        assert replacement_matches_expectation
+
+    @pytest.mark.usefixtures("add_store_name_outside_of_attached_attribute")
+    def test_if_store_names_outside_of_attached_attributes_are_ignored(self, replacement_matches_expectation):
+        assert replacement_matches_expectation
 
 
-@pytest.fixture
-def original_definition(original_definition_lines):
-    return "\n".join(original_definition_lines)
+class TestGetPartTableClasses:
+    @pytest.fixture
+    def part_table_classes(self):
+        return dict(PartA=type("PartA", (Part,), dict()))
 
+    @pytest.fixture
+    def other_attrs(self):
+        return dict()
 
-@pytest.fixture
-def expected_definition_lines():
-    return ["pa : attach@replacement_pa_store"]
+    @pytest.fixture
+    def attrs(self, part_table_classes, other_attrs):
+        return {**part_table_classes, **other_attrs}
 
+    @pytest.fixture
+    def table_cls(self, attrs):
+        class Table:
+            pass
 
-@pytest.fixture
-def expected_definition(expected_definition_lines):
-    return "\n".join(expected_definition_lines)
+        for name, attr in attrs.items():
+            setattr(Table, name, attr)
+        return Table
 
+    @pytest.fixture
+    def add_part_table_class(self, part_table_classes):
+        part_table_classes["PartB"] = type("PartB", (Part,), dict())
 
-@pytest.fixture
-def stores():
-    return dict(original_pa_store="replacement_pa_store")
+    @pytest.fixture
+    def ignored_parts(self):
+        return []
 
+    @pytest.fixture
+    def add_ignored_part_table(self, other_attrs, ignored_parts):
+        name = "IgnoredPart"
+        other_attrs[name] = type(name, (Part,), dict())
+        ignored_parts.append(name)
 
-@pytest.fixture
-def add_store(original_definition_lines, expected_definition_lines, stores):
-    original_definition_lines.append("pb: attach@original_pb_store")
-    expected_definition_lines.append("pb: attach@replacement_pb_store")
-    stores["original_pb_store"] = "replacement_pb_store"
+    @pytest.fixture
+    def add_non_part_class(self, other_attrs):
+        name = "NotAPart"
+        other_attrs[name] = type(name, tuple(), dict())
 
+    @pytest.fixture
+    def add_non_class_attr(self, other_attrs):
+        other_attrs["NotAClass"] = "NotAClass"
 
-@pytest.fixture
-def add_store_name_outside_of_attached_attribute(original_definition_lines, expected_definition_lines, stores):
-    original_definition_lines.append("pc: int # original_pc_store")
-    expected_definition_lines.append("pc: int # original_pc_store")
-    stores["original_pc_store"] = "replacement_pc_store"
+    @pytest.fixture
+    def correct_part_tables_returned(self, table_cls, part_table_classes, ignored_parts):
+        return dj_helpers.get_part_table_classes(table_cls, ignored_parts=ignored_parts) == part_table_classes
 
+    @pytest.fixture
+    def add_lowercase_part_table_class(self, other_attrs):
+        name = "lowercase_part"
+        other_attrs[name] = type(name, (Part,), dict())
 
-@pytest.fixture
-def replaced_definition_matches_expected_definition(original_definition, expected_definition, stores):
-    return dj_helpers.replace_stores(original_definition, stores) == expected_definition
+    def test_if_single_part_table_is_found(self, correct_part_tables_returned):
+        assert correct_part_tables_returned
 
+    @pytest.mark.usefixtures("add_part_table_class")
+    def test_if_multiple_part_tables_are_found(self, correct_part_tables_returned):
+        assert correct_part_tables_returned
 
-def test_if_single_store_is_replaced(replaced_definition_matches_expected_definition):
-    assert replaced_definition_matches_expected_definition
+    @pytest.mark.usefixtures("add_ignored_part_table")
+    def test_if_ignored_part_tables_are_ignored(self, correct_part_tables_returned):
+        assert correct_part_tables_returned
 
+    @pytest.mark.usefixtures("add_non_part_class")
+    def test_if_non_part_classes_are_ignored(self, correct_part_tables_returned):
+        assert correct_part_tables_returned
 
-@pytest.mark.usefixtures("add_store")
-def test_if_multiple_stores_are_replaced(replaced_definition_matches_expected_definition):
-    assert replaced_definition_matches_expected_definition
+    @pytest.mark.usefixtures("add_non_class_attr")
+    def test_if_non_class_attrs_are_ignored(self, correct_part_tables_returned):
+        assert correct_part_tables_returned
 
-
-@pytest.mark.usefixtures("add_store_name_outside_of_attached_attribute")
-def test_if_store_names_outside_of_attached_attributes_are_ignored(replaced_definition_matches_expected_definition):
-    assert replaced_definition_matches_expected_definition
+    @pytest.mark.usefixtures("add_lowercase_part_table_class")
+    def test_if_lowercase_part_table_classes_are_ignored(self, correct_part_tables_returned):
+        assert correct_part_tables_returned
