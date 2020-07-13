ee0df6724fb29d8d42f68dcf4f4ada5e7432ac9b
christoph <chris24.blessing@gmail.com>
2020-07-13 12:43:40 +0200

    Implement 'create_table_cls' method

--- link/external/local.py
+++ link/external/local.py
@@ -1,5 +1,6 @@
 from typing import Type
 
+from datajoint import Part
 from datajoint.table import Table
 
 from .outbound import OutboundTableFactory
@@ -7,6 +8,8 @@ from .source import SourceTableFactory
 
 
 class LocalTableFactory(OutboundTableFactory):
+    replace_stores = None
+
     def __init__(self, table_cls: Type[Table], source_table_factory: SourceTableFactory) -> None:
         super().__init__(table_cls)
         self.source_table_factory = source_table_factory
@@ -17,7 +20,12 @@ class LocalTableFactory(OutboundTableFactory):
         return type(self.table_name, (self.table_cls, local_table_cls), dict())
 
     def create_table_cls(self) -> Type[Table]:
-        pass
+        local_table_cls = super().create_table_cls()
+        local_table_cls.definition = self.replace_stores(str(self.source_table_factory().heading))
+        for name, part in self.source_table_factory().parts.items():
+            part_definition = "-> master\n" + self.replace_stores(str(part.heading))
+            setattr(local_table_cls, name, type(name, (Part,), dict(definition=part_definition)))
+        return local_table_cls
 
     def __repr__(self) -> str:
         return f"{self.__class__.__qualname__}({self.table_cls}, {self.source_table_factory})"
--- tests/unit/external/test_external_local.py
+++ tests/unit/external/test_external_local.py
@@ -1,6 +1,7 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 import pytest
+from datajoint import Part
 
 from link.external.outbound import OutboundTableFactory
 
@@ -16,15 +17,56 @@ def factory_args(source_table_factory, created_table_cls):
 
 
 @pytest.fixture
-def source_table_factory():
+def make_heading():
+    def _make_heading(prefix):
+        name = prefix + "_heading"
+        heading = MagicMock(name=name)
+        heading.__str__ = MagicMock(name=name + ".__str__", return_value=name)
+        return heading
+
+    return _make_heading
+
+
+@pytest.fixture
+def source_table_part_names():
+    return ["PartA", "PartB", "PartC"]
+
+
+@pytest.fixture
+def source_table_parts(make_heading, source_table_part_names):
+    return {name: type(name, tuple(), dict(heading=make_heading(name))) for name in source_table_part_names}
+
+
+@pytest.fixture
+def source_table(make_heading, source_table_parts):
+    return MagicMock(name="source_table", heading=make_heading("source_table"), parts=source_table_parts)
+
+
+@pytest.fixture
+def source_table_factory(source_table):
     name = "source_table_factory"
-    source_table_factory = MagicMock(name=name)
+    source_table_factory = MagicMock(name=name, return_value=source_table)
     source_table_factory.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
     return source_table_factory
 
 
+@pytest.fixture
+def replace_stores():
+    return MagicMock(name="replace_stores", side_effect=lambda heading: heading + "_with_replaced_stores")
+
+
+@pytest.fixture
+def configure_kwargs(configure_kwargs, replace_stores):
+    configure_kwargs["replace_stores"] = replace_stores
+    return configure_kwargs
+
+
 def test_if_subclass_of_outbound_table_factory(factory_cls):
     assert issubclass(factory_cls, OutboundTableFactory)
+
+
+def test_if_replace_stores_is_none(factory):
+    assert factory.replace_stores is None
 
 
 def test_if_source_table_factory_is_stored_as_instance_attribute(factory, source_table_factory):
@@ -41,6 +83,37 @@ class TestSpawnTableCls:
 
     def test_if_name_attribute_of_returned_class_is_correctly_set(self, factory, table_name):
         assert factory.spawn_table_cls().__name__ == table_name
+
+
+@pytest.mark.usefixtures("configure")
+class TestCreateTableCls:
+    def test_if_replace_stores_is_called_correctly(self, factory, replace_stores):
+        factory.create_table_cls()
+        assert replace_stores.mock_calls == [
+            call("source_table_heading"),
+            call("PartA_heading"),
+            call("PartB_heading"),
+            call("PartC_heading"),
+        ]
+
+    def test_if_source_table_heading_with_replaced_stores_is_set_as_definition_of_created_table_cls(self, factory):
+        assert factory.create_table_cls().definition == "source_table_heading_with_replaced_stores"
+
+    def test_if_source_part_tables_are_assigned_to_created_table_cls(self, factory, source_table_part_names):
+        local_table_cls = factory.create_table_cls()
+        assert all(hasattr(local_table_cls, part_name) for part_name in source_table_part_names)
+
+    def test_if_part_tables_are_part_tables(self, factory, source_table_part_names):
+        local_table_cls = factory.create_table_cls()
+        for part_name in source_table_part_names:
+            part = getattr(local_table_cls, part_name)
+            assert issubclass(part, Part)
+
+    def test_if_definition_of_part_tables_is_correct(self, factory, source_table_part_names):
+        local_table_cls = factory.create_table_cls()
+        for part_name in source_table_part_names:
+            part = getattr(local_table_cls, part_name)
+            assert part.definition == f"-> master\n{part_name}_heading_with_replaced_stores"
 
 
 def test_repr(factory, created_table_cls):
