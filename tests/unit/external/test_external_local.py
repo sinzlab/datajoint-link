73b4224021b007178154f53949258f6a4c9dd6c1
christoph <chris24.blessing@gmail.com>
2020-07-13 13:18:00 +0200

    Refactor tests

--- tests/unit/external/test_external_local.py
+++ tests/unit/external/test_external_local.py
@@ -87,6 +87,10 @@ class TestSpawnTableCls:
 
 @pytest.mark.usefixtures("configure")
 class TestCreateTableCls:
+    @pytest.fixture
+    def local_table_cls(self, factory):
+        return factory.create_table_cls()
+
     def test_if_replace_stores_is_called_correctly(self, factory, replace_stores):
         factory.create_table_cls()
         assert replace_stores.mock_calls == [
@@ -100,30 +104,27 @@ class TestCreateTableCls:
         factory.create_table_cls()
         assert source_table_factory.mock_calls == [call(), call(), call()]
 
-    def test_if_source_table_heading_with_replaced_stores_is_set_as_definition_of_created_table_cls(self, factory):
-        assert factory.create_table_cls().definition == "source_table_heading_with_replaced_stores"
+    def test_if_source_table_heading_with_replaced_stores_is_set_as_definition_of_created_table_cls(
+        self, local_table_cls
+    ):
+        assert local_table_cls.definition == "source_table_heading_with_replaced_stores"
 
-    def test_if_source_part_tables_are_assigned_to_created_table_cls(self, factory, source_table_part_names):
-        local_table_cls = factory.create_table_cls()
+    def test_if_source_part_tables_are_assigned_to_created_table_cls(self, local_table_cls, source_table_part_names):
         assert all(hasattr(local_table_cls, part_name) for part_name in source_table_part_names)
 
     def test_if_references_to_assigned_part_table_classes_is_stored_in_parts_attribute(
-        self, factory, source_table_part_names
+        self, local_table_cls, source_table_part_names
     ):
-        local_table_cls = factory.create_table_cls()
         assert all(name in local_table_cls.parts for name in source_table_part_names)
 
-    def test_if_part_tables_are_part_tables(self, factory, source_table_part_names):
-        local_table_cls = factory.create_table_cls()
-        for part_name in source_table_part_names:
-            part = getattr(local_table_cls, part_name)
-            assert issubclass(part, Part)
+    def test_if_part_tables_are_part_tables(self, local_table_cls, source_table_part_names):
+        assert all(issubclass(part, Part) for part in local_table_cls.parts.values())
 
-    def test_if_definition_of_part_tables_is_correct(self, factory, source_table_part_names):
-        local_table_cls = factory.create_table_cls()
-        for part_name in source_table_part_names:
-            part = getattr(local_table_cls, part_name)
-            assert part.definition == f"-> master\n{part_name}_heading_with_replaced_stores"
+    def test_if_definition_of_part_tables_is_correct(self, local_table_cls, source_table_part_names):
+        assert all(
+            part.definition == f"-> master\n{name}_heading_with_replaced_stores"
+            for name, part in local_table_cls.parts.items()
+        )
 
 
 def test_repr(factory, created_table_cls):
