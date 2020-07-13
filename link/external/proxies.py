cc4ce2c76da07bfef698253991db68044aa3ab4f
christoph <chris24.blessing@gmail.com>
2020-07-13 17:40:58 +0200

    Rename "table" to "table_factory"

--- link/external/proxies.py
+++ link/external/proxies.py
@@ -5,61 +5,61 @@ PrimaryKey = Dict[str, Union[str, int, float]]
 
 
 class SourceTableProxy:
-    def __init__(self, table):
-        self.table = table
+    def __init__(self, table_factory):
+        self.table_factory = table_factory
 
     @property
     def primary_attr_names(self) -> List[str]:
-        return self.table.heading.primary_key
+        return self.table_factory.heading.primary_key
 
     @property
     def primary_keys(self) -> List[PrimaryKey]:
-        return self.table().proj().fetch(as_dict=True)
+        return self.table_factory().proj().fetch(as_dict=True)
 
     def get_primary_keys_in_restriction(self, restriction) -> List[PrimaryKey]:
-        return (self.table().proj() & restriction).fetch(as_dict=True)
+        return (self.table_factory().proj() & restriction).fetch(as_dict=True)
 
     def fetch(self, primary_keys: List[PrimaryKey]) -> Dict[str, Any]:
-        entities = dict(main=(self.table() & primary_keys).fetch(as_dict=True))
+        entities = dict(main=(self.table_factory() & primary_keys).fetch(as_dict=True))
         entities["parts"] = dict()
-        for name, part in self.table.parts.items():
+        for name, part in self.table_factory.parts.items():
             entities["parts"][name] = (part & primary_keys).fetch(as_dict=True)
         return entities
 
     def __repr__(self) -> str:
-        return self.__class__.__qualname__ + "(" + repr(self.table) + ")"
+        return self.__class__.__qualname__ + "(" + repr(self.table_factory) + ")"
 
 
 class NonSourceTableProxy(SourceTableProxy):
     @property
     def deletion_requested(self) -> List[PrimaryKey]:
-        return self.table().DeletionRequested.fetch(as_dict=True)
+        return self.table_factory().DeletionRequested.fetch(as_dict=True)
 
     def delete(self, primary_keys: List[PrimaryKey]) -> None:
-        (self.table() & primary_keys).delete()
+        (self.table_factory() & primary_keys).delete()
 
     def insert(self, entities: Dict[str, Any]) -> None:
-        self.table().insert(entities["main"])
+        self.table_factory().insert(entities["main"])
         for part_name, part_entities in entities["parts"].items():
-            self.table.parts[part_name].insert(part_entities)
+            self.table_factory.parts[part_name].insert(part_entities)
 
     def start_transaction(self) -> None:
-        self.table().connection.start_transaction()
+        self.table_factory().connection.start_transaction()
 
     def commit_transaction(self) -> None:
-        self.table().connection.commit_transaction()
+        self.table_factory().connection.commit_transaction()
 
     def cancel_transaction(self) -> None:
-        self.table().connection.cancel_transaction()
+        self.table_factory().connection.cancel_transaction()
 
 
 class OutboundTableProxy(NonSourceTableProxy):
     @property
     def deletion_approved(self) -> List[PrimaryKey]:
-        return self.table().DeletionApproved.fetch(as_dict=True)
+        return self.table_factory().DeletionApproved.fetch(as_dict=True)
 
     def approve_deletion(self, primary_keys: List[PrimaryKey]) -> None:
-        self.table().DeletionApproved.insert(primary_keys)
+        self.table_factory().DeletionApproved.insert(primary_keys)
 
 
 class LocalTableProxy(NonSourceTableProxy):
--- tests/unit/external/test_proxies.py
+++ tests/unit/external/test_proxies.py
@@ -52,23 +52,28 @@ def parts(part_names, part_entities):
 
 
 @pytest.fixture
-def table(primary_attr_names, primary_keys, main_entities, parts):
-    name = "table"
-    table = MagicMock(name=name)
-    table.heading.primary_key = primary_attr_names
-    table.return_value.proj.return_value.fetch.return_value = primary_keys
-    table.return_value.proj.return_value.__and__.return_value.fetch.return_value = primary_keys
-    table.return_value.__and__.return_value.fetch.return_value = main_entities
-    table.return_value.DeletionRequested.fetch.return_value = primary_keys
-    table.return_value.DeletionApproved.fetch.return_value = primary_keys
-    table.parts = parts
-    table.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+def table(primary_keys, main_entities):
+    table = MagicMock(name="table")
+    table.proj.return_value.fetch.return_value = primary_keys
+    table.proj.return_value.__and__.return_value.fetch.return_value = primary_keys
+    table.__and__.return_value.fetch.return_value = main_entities
+    table.DeletionRequested.fetch.return_value = primary_keys
+    table.DeletionApproved.fetch.return_value = primary_keys
     return table
 
 
 @pytest.fixture
-def proxy(proxy_cls, table):
-    return proxy_cls(table)
+def table_factory(primary_attr_names, parts, table):
+    name = "table_factory"
+    table_factory = MagicMock(name=name, return_value=table)
+    table_factory.parts = parts
+    table_factory.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+    return table_factory
+
+
+@pytest.fixture
+def proxy(proxy_cls, table_factory):
+    return proxy_cls(table_factory)
 
 
 class TestSourceTableProxy:
@@ -84,43 +89,43 @@ class TestSourceTableProxy:
     def restriction(self):
         return "restriction"
 
-    def test_if_table_is_stored_as_instance_attribute(self, table, proxy):
-        assert proxy.table is table
+    def test_if_table_factory_is_stored_as_instance_attribute(self, table_factory, proxy):
+        assert proxy.table_factory is table_factory
 
-    def test_if_table_is_instantiated_when_accessing_primary_keys_property(self, table, proxy):
+    def test_if_table_is_instantiated_when_accessing_primary_keys_property(self, table_factory, proxy):
         _ = proxy.primary_keys
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_table_is_projected_to_primary_keys_when_accessing_primary_keys_property(self, table, proxy):
         _ = proxy.primary_keys
-        table.return_value.proj.assert_called_once_with()
+        table.proj.assert_called_once_with()
 
     def test_if_fetch_is_called_correctly_when_accessing_primary_keys_property(self, table, proxy):
         _ = proxy.primary_keys
-        table.return_value.proj.return_value.fetch.assert_called_once_with(as_dict=True)
+        table.proj.return_value.fetch.assert_called_once_with(as_dict=True)
 
     def test_if_primary_keys_property_returns_correct_value(self, primary_keys, proxy):
         assert proxy.primary_keys == primary_keys
 
-    def test_if_table_is_instantiated_when_getting_primary_keys_in_restriction(self, table, proxy, restriction):
+    def test_if_table_is_instantiated_when_getting_primary_keys_in_restriction(self, table_factory, proxy, restriction):
         proxy.get_primary_keys_in_restriction(restriction)
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_table_is_projected_to_primary_keys_when_getting_primary_keys_in_restriction(
         self, table, proxy, restriction
     ):
         proxy.get_primary_keys_in_restriction(restriction)
-        table.return_value.proj.assert_called_once_with()
+        table.proj.assert_called_once_with()
 
     def test_if_projected_table_is_restricted_when_getting_primary_keys_in_restriction(self, table, proxy, restriction):
         proxy.get_primary_keys_in_restriction(restriction)
-        table.return_value.proj.return_value.__and__.assert_called_once_with(restriction)
+        table.proj.return_value.__and__.assert_called_once_with(restriction)
 
     def test_if_fetch_on_restricted_table_is_called_correctly_when_getting_primary_keys_in_restriction(
         self, table, proxy, restriction
     ):
         proxy.get_primary_keys_in_restriction(restriction)
-        table.return_value.proj.return_value.__and__.return_value.fetch.assert_called_once_with(as_dict=True)
+        table.proj.return_value.__and__.return_value.fetch.assert_called_once_with(as_dict=True)
 
     def test_if_correct_primary_keys_are_returned_when_getting_primary_keys_in_restriction(
         self, primary_keys, table, proxy, restriction
@@ -128,16 +133,16 @@ class TestSourceTableProxy:
         assert proxy.get_primary_keys_in_restriction(restriction) == primary_keys
 
     @pytest.mark.usefixtures("fetch")
-    def test_if_table_is_instantiated_when_fetching_entities(self, table):
-        table.assert_called_once_with()
+    def test_if_table_is_instantiated_when_fetching_entities(self, table_factory):
+        table_factory.assert_called_once_with()
 
     @pytest.mark.usefixtures("fetch")
     def test_if_table_is_restricted_when_fetching_entities(self, primary_keys, table):
-        table.return_value.__and__.assert_called_once_with(primary_keys)
+        table.__and__.assert_called_once_with(primary_keys)
 
     @pytest.mark.usefixtures("fetch")
     def test_if_entities_are_correctly_fetched_from_restricted_table(self, table):
-        table.return_value.__and__.return_value.fetch.assert_called_once_with(as_dict=True)
+        table.__and__.return_value.fetch.assert_called_once_with(as_dict=True)
 
     @pytest.mark.usefixtures("fetch")
     def test_if_part_tables_are_restricted_when_fetching_entities(self, primary_keys, parts):
@@ -153,7 +158,7 @@ class TestSourceTableProxy:
         assert proxy.fetch(primary_keys) == entities
 
     def test_repr(self, proxy):
-        assert repr(proxy) == "SourceTableProxy(table)"
+        assert repr(proxy) == "SourceTableProxy(table_factory)"
 
 
 class TestNonSourceTableProxy:
@@ -168,65 +173,65 @@ class TestNonSourceTableProxy:
     def delete(self, primary_keys, proxy):
         proxy.delete(primary_keys)
 
-    def test_if_table_is_instantiated_when_fetching_deletion_requested_entities(self, table, proxy):
+    def test_if_table_is_instantiated_when_fetching_deletion_requested_entities(self, table_factory, proxy):
         _ = proxy.deletion_requested
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_primary_keys_of_deletion_requested_entities_are_fetched_correctly(self, table, proxy):
         _ = proxy.deletion_requested
-        table.return_value.DeletionRequested.fetch.assert_called_once_with(as_dict=True)
+        table.DeletionRequested.fetch.assert_called_once_with(as_dict=True)
 
     def test_if_primary_keys_of_deletion_requested_entities_are_returned(self, primary_keys, proxy):
         assert proxy.deletion_requested == primary_keys
 
     @pytest.mark.usefixtures("delete")
-    def test_if_table_is_instantiated_when_deleting_entities(self, table):
-        table.assert_called_once_with()
+    def test_if_table_is_instantiated_when_deleting_entities(self, table_factory):
+        table_factory.assert_called_once_with()
 
     @pytest.mark.usefixtures("delete")
     def test_if_table_is_restricted_when_deleting_entities(self, primary_keys, table):
-        table.return_value.__and__.assert_called_once_with(primary_keys)
+        table.__and__.assert_called_once_with(primary_keys)
 
     @pytest.mark.usefixtures("delete")
     def test_if_entities_are_correctly_deleted_from_restricted_table(self, primary_keys, table):
-        table.return_value.__and__.return_value.delete.assert_called_once_with()
+        table.__and__.return_value.delete.assert_called_once_with()
 
-    def test_if_table_is_instantiated_when_inserting_entities(self, entities, table, proxy):
+    def test_if_table_is_instantiated_when_inserting_entities(self, entities, table_factory, proxy):
         proxy.insert(entities)
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
-    def test_if_entities_are_correctly_inserted(self, entities, main_entities, table, proxy):
+    def test_if_entities_are_correctly_inserted(self, entities, main_entities, table_factory, proxy):
         proxy.insert(entities)
-        table.return_value.insert.assert_called_once_with(main_entities)
+        table_factory.return_value.insert.assert_called_once_with(main_entities)
 
     def test_if_part_entities_are_inserted_into_part_tables(self, entities, parts, part_entities, proxy):
         proxy.insert(entities)
         for part, part_entities in zip(parts.values(), part_entities):
             part.insert.assert_called_once_with(part_entities)
 
-    def test_if_table_is_instantiated_when_starting_transaction(self, table, proxy):
+    def test_if_table_is_instantiated_when_starting_transaction(self, table_factory, proxy):
         proxy.start_transaction()
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_transaction_is_started_in_table(self, table, proxy):
         proxy.start_transaction()
-        table.return_value.connection.start_transaction.assert_called_once_with()
+        table.connection.start_transaction.assert_called_once_with()
 
-    def test_if_table_is_instantiated_when_committing_transaction(self, table, proxy):
+    def test_if_table_is_instantiated_when_committing_transaction(self, table_factory, proxy):
         proxy.commit_transaction()
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_transaction_is_committed_in_table(self, table, proxy):
         proxy.commit_transaction()
-        table.return_value.connection.commit_transaction.assert_called_once_with()
+        table.connection.commit_transaction.assert_called_once_with()
 
-    def test_if_table_is_instantiated_when_cancelling_transaction(self, table, proxy):
+    def test_if_table_is_instantiated_when_cancelling_transaction(self, table_factory, proxy):
         proxy.cancel_transaction()
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_transaction_is_cancelled_in_table(self, table, proxy):
         proxy.cancel_transaction()
-        table.return_value.connection.cancel_transaction.assert_called_once_with()
+        table.connection.cancel_transaction.assert_called_once_with()
 
 
 class TestOutboundTableProxy:
@@ -237,24 +242,24 @@ class TestOutboundTableProxy:
     def proxy_cls(self):
         return proxies.OutboundTableProxy
 
-    def test_if_table_is_instantiated_when_fetching_deletion_approved_entities(self, table, proxy):
+    def test_if_table_is_instantiated_when_fetching_deletion_approved_entities(self, table_factory, proxy):
         _ = proxy.deletion_approved
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_primary_keys_of_deletion_approved_entities_are_fetched_correctly(self, table, proxy):
         _ = proxy.deletion_approved
-        table.return_value.DeletionApproved.fetch.assert_called_once_with(as_dict=True)
+        table.DeletionApproved.fetch.assert_called_once_with(as_dict=True)
 
     def test_if_primary_keys_of_deletion_approved_entities_are_returned(self, primary_keys, proxy):
         assert proxy.deletion_approved == primary_keys
 
-    def test_if_table_is_instantiated_when_approving_deletion(self, primary_keys, table, proxy):
+    def test_if_table_is_instantiated_when_approving_deletion(self, primary_keys, table_factory, proxy):
         proxy.approve_deletion(primary_keys)
-        table.assert_called_once_with()
+        table_factory.assert_called_once_with()
 
     def test_if_primary_keys_are_inserted_into_deletion_approved_part_table(self, primary_keys, table, proxy):
         proxy.approve_deletion(primary_keys)
-        table.return_value.DeletionApproved.insert.assert_called_once_with(primary_keys)
+        table.DeletionApproved.insert.assert_called_once_with(primary_keys)
 
 
 def test_if_local_table_proxy_is_subclass_of_non_source_table_proxy():
