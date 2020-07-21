82ccb4b63bea561ffddcbee52583ca64351939ef
christoph <chris24.blessing@gmail.com>
2020-07-21 11:36:31 +0200

    Implement repository and its corresponding factory

--- link/entities/repository.py
+++ link/entities/repository.py
@@ -1,8 +1,38 @@
 from typing import Optional, Dict
 from dataclasses import dataclass, field
 
+from .contents import Contents
+from .flag_manager import FlagManagerFactory
+from .transaction_manager import TransactionManager
+from .gateway import AbstractGateway
+
 
 @dataclass
 class Entity:
     identifier: str
     flags: Optional[Dict[str, bool]] = field(default_factory=dict)
+
+
+@dataclass
+class Repository:
+    contents: Contents
+    flags: FlagManagerFactory
+    transaction: TransactionManager
+
+
+class RepositoryFactory:
+    def __init__(self, gateway: AbstractGateway, storage: Dict) -> None:
+        self.gateway = gateway
+        self.storage = storage
+
+    def __call__(self) -> Repository:
+        """Creates a repository."""
+        entities = {
+            identifier: Entity(identifier, self.gateway.get_flags(identifier))
+            for identifier in self.gateway.identifiers
+        }
+        return Repository(
+            contents=Contents(entities, self.gateway, self.storage),
+            flags=FlagManagerFactory(entities, self.gateway),
+            transaction=TransactionManager(entities, self.gateway),
+        )
--- tests/unit/entities/test_repository.py
+++ tests/unit/entities/test_repository.py
@@ -1,6 +1,11 @@
+from unittest.mock import call
+
 import pytest
 
-from link.entities.repository import Entity
+from link.entities.repository import Entity, Repository, RepositoryFactory
+from link.entities.contents import Contents
+from link.entities.flag_manager import FlagManagerFactory
+from link.entities.transaction_manager import TransactionManager
 
 
 class TestEntity:
@@ -19,3 +24,58 @@ class TestEntity:
 
     def test_repr(self, identifier, flags):
         assert repr(Entity(identifier, flags=flags)) == "Entity(identifier='identifier0', flags={'flag': True})"
+
+
+class TestRepositoryFactory:
+    @pytest.fixture
+    def storage(self):
+        return dict()
+
+    @pytest.fixture
+    def factory(self, gateway_spy, storage):
+        return RepositoryFactory(gateway_spy, storage)
+
+    def test_if_gateway_is_stored_as_instance_attribute(self, factory, gateway_spy):
+        assert factory.gateway is gateway_spy
+
+    def test_if_storage_is_stored_as_instance_attribute(self, factory, storage):
+        assert factory.storage is storage
+
+    def test_if_flags_are_retrieved_from_gateway(self, identifiers, factory, gateway_spy):
+        _ = factory()
+        assert gateway_spy.get_flags.call_args_list == [call(identifier) for identifier in identifiers]
+
+    def test_if_repository_is_returned(self, factory):
+        assert isinstance(factory(), Repository)
+
+    def test_if_contents_is_assigned_to_contents_attribute_of_returned_repository(self, factory):
+        assert isinstance(factory().contents, Contents)
+
+    def test_if_entities_associated_with_contents_are_correct(self, factory, entities):
+        assert factory().contents.entities == entities
+
+    def test_if_gateway_of_contents_is_correct(self, factory, gateway_spy):
+        assert factory().contents.gateway is gateway_spy
+
+    def test_if_storage_of_contents_is_correct(self, factory, storage):
+        assert factory().contents.storage is storage
+
+    def test_if_flag_manager_factory_is_assigned_to_flags_attribute_of_returned_repository(self, factory):
+        assert isinstance(factory().flags, FlagManagerFactory)
+
+    def test_if_entities_associated_with_contents_and_flag_manager_factory_are_identical(self, factory):
+        repo = factory()
+        assert all(entity is repo.flags.entities[identifier] for identifier, entity in repo.contents.items())
+
+    def test_if_gateway_of_flag_manager_factory_is_correct(self, factory, gateway_spy):
+        assert factory().flags.gateway is gateway_spy
+
+    def test_if_transaction_manager_is_assigned_to_transaction_attribute_of_returned_repository(self, factory):
+        assert isinstance(factory().transaction, TransactionManager)
+
+    def test_if_entities_associated_with_contents_and_transaction_manager_are_identical(self, factory):
+        repo = factory()
+        assert all(entity is repo.transaction.entities[identifier] for identifier, entity in repo.contents.items())
+
+    def test_if_gateway_of_transaction_manager_is_correct(self, factory, gateway_spy):
+        assert factory().transaction.gateway is gateway_spy
