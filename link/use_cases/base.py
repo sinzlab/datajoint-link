3c1677e9af21aab6350ef88781d9d3fd17f2abf9
christoph <chris24.blessing@gmail.com>
2020-07-21 14:07:25 +0200

    Use new repository link factory

--- link/use_cases/base.py
+++ link/use_cases/base.py
@@ -1,23 +1,30 @@
 """Contains the abstract base classes use-cases inherit from."""
+from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Callable, Any
+from typing import TYPE_CHECKING, Callable, Any
+
+from ..entities.representation import _represent
+
+if TYPE_CHECKING:
+    from . import RepositoryLink, RepositoryLinkFactory
 
 
 class UseCase(ABC):
     """Specifies the interface for use-cases."""
 
-    def __init__(self, output_port: Callable[[Any], None]) -> None:
+    def __init__(self, repo_link_factory: RepositoryLinkFactory, output_port: Callable[[Any], None]) -> None:
         """Initializes the use-case."""
+        self.repo_link_factory = repo_link_factory
         self.output_port = output_port
 
     def __call__(self, *args, **kwargs) -> None:
         """Executes the use-case and passes its output to the output port."""
-        output = self.execute(*args, **kwargs)
+        output = self.execute(self.repo_link_factory(dict()), *args, **kwargs)
         self.output_port(output)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}({self.output_port})"
+        return _represent(self, ["repo_link_factory", "output_port"])
 
     @abstractmethod
-    def execute(self, *args, **kwargs):
+    def execute(self, repo_link: RepositoryLink, *args, **kwargs):
         """Executes the use-case."""
--- tests/unit/use_cases/test_base.py
+++ tests/unit/use_cases/test_base.py
@@ -1,42 +1,58 @@
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock
 
 import pytest
 
-from link.use_cases import base
+from link.use_cases import RepositoryLink, RepositoryLinkFactory, base
 
 
 @pytest.fixture
-def output_port():
-    name = "output_port"
-    output_port = MagicMock(name=name)
-    output_port.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
-    return output_port
+def repo_link_factory_spy():
+    name = "repo_link_factory_spy"
+    link_factory_spy = MagicMock(name=name, return_value="link", spec=RepositoryLinkFactory)
+    link_factory_spy.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+    return link_factory_spy
 
 
 @pytest.fixture
-def execute_call():
-    return call("arg1", "arg2", kwarg1="kwarg1", kwarg2="kwarg2")
+def output_port_spy():
+    name = "output_port_spy"
+    output_port_spy = MagicMock(name=name)
+    output_port_spy.__repr__ = MagicMock(name=name + ".__repr", return_value=name)
+    return output_port_spy
 
 
-class TestInitializationUseCase:
-    @pytest.fixture
-    def use_case(self, output_port):
-        class UseCase(base.UseCase):
-            execute = MagicMock(name="execute", return_value="executed!")
-            __qualname__ = "UseCase"
+@pytest.fixture
+def use_case(repo_link_factory_spy, output_port_spy):
+    class UseCase(base.UseCase):
+        def execute(self, repo_link: RepositoryLink, *args, **kwargs):
+            pass
 
-        return UseCase(output_port)
+    UseCase.__qualname__ = UseCase.__name__
+    UseCase.execute = MagicMock(name=UseCase.__name__ + ".execute", return_value="output")
+    return UseCase(repo_link_factory_spy, output_port_spy)
 
-    def test_if_output_port_is_stored_as_instance_attribute_when_initializing(self, use_case, output_port):
-        assert use_case.output_port is output_port
 
-    def test_if_execute_method_is_correctly_called_when_calling_use_case(self, use_case, execute_call):
-        use_case(*execute_call.args, **execute_call.kwargs)
-        use_case.execute.assert_called_once_with(*execute_call.args, **execute_call.kwargs)
+class TestInit:
+    def test_if_repo_link_factory_is_stored_as_instance_attribute(self, use_case, repo_link_factory_spy):
+        assert use_case.repo_link_factory is repo_link_factory_spy
 
-    def test_if_output_port_is_correctly_called_when_calling_use_case(self, use_case, output_port):
+    def test_if_output_port_is_stored_as_instance_attribute(self, use_case, output_port_spy):
+        assert use_case.output_port is output_port_spy
+
+
+class TestCall:
+    def test_if_repo_link_factory_is_called_correctly(self, use_case, repo_link_factory_spy):
         use_case()
-        output_port.assert_called_once_with("executed!")
+        repo_link_factory_spy.assert_called_once_with(dict())
 
-    def test_repr(self, use_case):
-        assert repr(use_case) == "UseCase(output_port)"
+    def test_if_call_to_execute_method_is_correct(self, use_case):
+        use_case("arg", kwarg="kwarg")
+        use_case.execute.assert_called_once_with("link", "arg", kwarg="kwarg")
+
+    def test_if_call_to_output_port_is_correct(self, use_case, output_port_spy):
+        use_case()
+        output_port_spy.assert_called_once_with("output")
+
+
+def test_repr(use_case):
+    assert repr(use_case) == "UseCase(repo_link_factory=repo_link_factory_spy, output_port=output_port_spy)"
