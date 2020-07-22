db314ff88b63dad781da42df45d29fe2d7e940c4
christoph <chris24.blessing@gmail.com>
2020-07-22 18:25:23 +0200

    Implement new gateway

---
+++ link/adapters/datajoint/gateway.py
@@ -0,0 +1,79 @@
+from typing import List, Dict, Any
+from itertools import tee
+
+from .proxy import AbstractTableProxy
+from .identification import IdentificationTranslator
+from ...entities.gateway import AbstractGateway
+from ...entities.representation import represent
+
+
+class DataJointGateway(AbstractGateway):
+    def __init__(self, table_proxy: AbstractTableProxy, translator: IdentificationTranslator) -> None:
+        self.table_proxy = table_proxy
+        self.translator = translator
+
+    @property
+    def identifiers(self) -> List[str]:
+        """Returns the identifiers of all entities in the table."""
+        return [self.translator.to_identifier(primary_key) for primary_key in self.table_proxy.primary_keys]
+
+    def get_flags(self, identifier: str) -> Dict[str, bool]:
+        """Gets the names and values of all flags that are set on the entity identified by the provided identifier."""
+        flags = self.table_proxy.get_flags(self.translator.to_primary_key(identifier))
+        return {self.to_flag_name(flag_table_name): flag for flag_table_name, flag in flags.items()}
+
+    def fetch(self, identifier: str) -> Any:
+        """Fetches the entity identified by the provided identifier."""
+        primary_key = self.translator.to_primary_key(identifier)
+        return dict(master=self.table_proxy.fetch_master(primary_key), parts=self.table_proxy.fetch_parts(primary_key))
+
+    def insert(self, data: Any) -> None:
+        """Inserts the provided entity into the table."""
+        self.table_proxy.insert_master(data["master"])
+        self.table_proxy.insert_parts(data["parts"])
+
+    def delete(self, identifier: str) -> None:
+        """Deletes the entity identified by the provided identifier from the table."""
+        self.table_proxy.delete(self.translator.to_primary_key(identifier))
+
+    def set_flag(self, identifier: str, flag: str, value: bool) -> None:
+        """Sets the flag on the entity identified by the provided identifier to the provided value."""
+        if value:
+            self._enable_flag(identifier, flag)
+        else:
+            self._disable_flag(identifier, flag)
+
+    def _enable_flag(self, identifier: str, flag: str) -> None:
+        self.table_proxy.enable_flag(self.translator.to_primary_key(identifier), self._to_flag_table_name(flag))
+
+    def _disable_flag(self, identifier: str, flag: str) -> None:
+        self.table_proxy.disable_flag(self.translator.to_primary_key(identifier), self._to_flag_table_name(flag))
+
+    def start_transaction(self) -> None:
+        """Starts a transaction in the table."""
+        self.table_proxy.start_transaction()
+
+    def commit_transaction(self) -> None:
+        """Commits a transaction in the table."""
+        self.table_proxy.commit_transaction()
+
+    def cancel_transaction(self) -> None:
+        """Cancels a transaction in the table."""
+        self.table_proxy.cancel_transaction()
+
+    @staticmethod
+    def to_flag_name(flag_table_name: str) -> str:
+        """Translates the provided flag table name to the corresponding flag name."""
+        indexes = [index for index, letter in enumerate(flag_table_name) if letter.isupper() and index != 0]
+        starts, stops = tee(indexes + [len(flag_table_name)])
+        next(stops, None)
+        parts = ["_" + flag_table_name[start:stop] for start, stop in zip(starts, stops)]
+        return "".join([flag_table_name[: indexes[0]]] + parts).lower()
+
+    @staticmethod
+    def _to_flag_table_name(flag_name: str) -> str:
+        """Translates the provided flag name to the corresponding flag table name."""
+        return "".join(part.title() for part in flag_name.split("_"))
+
+    def __repr__(self) -> str:
+        return represent(self, ["table_proxy", "translator"])
---
+++ tests/unit/adapters/datajoint/test_gateway.py
@@ -0,0 +1,180 @@
+from unittest.mock import MagicMock, call
+
+import pytest
+
+from link.entities.gateway import AbstractGateway
+from link.adapters.datajoint.gateway import DataJointGateway
+from link.adapters.datajoint.proxy import AbstractTableProxy
+from link.adapters.datajoint.identification import IdentificationTranslator
+
+
+def test_if_datajoint_gateway_is_subclass_of_abstract_gateway():
+    assert issubclass(DataJointGateway, AbstractGateway)
+
+
+@pytest.fixture
+def primary_keys():
+    return ["primary_key" + str(i) for i in range(3)]
+
+
+@pytest.fixture
+def flags():
+    return dict(SuperLongAndComplexFlag=True, AnotherEvenLongerAndMoreComplexFlag=False)
+
+
+@pytest.fixture
+def table_proxy_spy(primary_keys, flags):
+    name = "table_proxy_spy"
+    table_proxy_spy = MagicMock(name=name, spec=AbstractTableProxy, primary_keys=primary_keys)
+    table_proxy_spy.get_flags.return_value = flags
+    table_proxy_spy.fetch_master.return_value = "master_entity"
+    table_proxy_spy.fetch_parts.return_value = "part_entities"
+    table_proxy_spy.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+    return table_proxy_spy
+
+
+@pytest.fixture
+def identifiers():
+    return ["identifier" + str(i) for i in range(3)]
+
+
+@pytest.fixture
+def translator_spy(identifiers):
+    name = "translator_spy"
+    translator_spy = MagicMock(name="translator_spy", spec=IdentificationTranslator)
+    translator_spy.to_identifier.side_effect = identifiers
+    translator_spy.to_primary_key.return_value = "primary_key0"
+    translator_spy.__repr__ = MagicMock(name=name + ".__repr__", return_value=name)
+    return translator_spy
+
+
+@pytest.fixture
+def gateway(table_proxy_spy, translator_spy):
+    return DataJointGateway(table_proxy_spy, translator_spy)
+
+
+class TestInit:
+    def test_if_table_proxy_is_stored_as_instance_attribute(self, gateway, table_proxy_spy):
+        assert gateway.table_proxy is table_proxy_spy
+
+    def test_if_translator_is_stored_as_instance_attribute(self, gateway, translator_spy):
+        assert gateway.translator is translator_spy
+
+
+class TestIdentifiersProperty:
+    def test_if_call_to_translator_is_correct(self, gateway, translator_spy, primary_keys):
+        _ = gateway.identifiers
+        assert translator_spy.to_identifier.call_args_list == [call(primary_key) for primary_key in primary_keys]
+
+    def test_if_identifiers_are_returned(self, gateway, identifiers):
+        assert gateway.identifiers == identifiers
+
+
+class TestGetFlags:
+    def test_if_call_to_translator_is_correct(self, gateway, translator_spy):
+        gateway.get_flags("identifier0")
+        translator_spy.to_primary_key.assert_called_once_with("identifier0")
+
+    def test_if_call_to_table_proxy_is_correct(self, gateway, table_proxy_spy):
+        gateway.get_flags("identifier0")
+        table_proxy_spy.get_flags.assert_called_once_with("primary_key0")
+
+    def test_if_correct_flags_are_returned(self, gateway):
+        flags = dict(super_long_and_complex_flag=True, another_even_longer_and_more_complex_flag=False)
+        assert gateway.get_flags("identifier0") == flags
+
+
+class TestFetch:
+    def test_if_call_to_translator_is_correct(self, gateway, translator_spy):
+        gateway.fetch("identifier0")
+        translator_spy.to_primary_key.assert_called_once_with("identifier0")
+
+    def test_if_call_to_fetch_master_method_of_table_proxy_is_correct(self, gateway, table_proxy_spy):
+        gateway.fetch("identifier0")
+        table_proxy_spy.fetch_master.assert_called_once_with("primary_key0")
+
+    def test_if_call_to_fetch_parts_method_of_table_proxy_is_correct(self, gateway, table_proxy_spy):
+        gateway.fetch("identifier0")
+        table_proxy_spy.fetch_parts.assert_called_once_with("primary_key0")
+
+    def test_if_returned_data_is_correct(self, gateway):
+        assert gateway.fetch("identifier0") == dict(master="master_entity", parts="part_entities")
+
+
+@pytest.fixture
+def data():
+    return dict(master="master_entity", parts="part_entities")
+
+
+class TestInsert:
+    def test_if_master_entity_is_inserted_into_proxy(self, gateway, table_proxy_spy, data):
+        gateway.insert(data)
+        table_proxy_spy.insert_master.assert_called_once_with(data["master"])
+
+    def test_if_part_entities_are_inserted_into_proxy(self, gateway, table_proxy_spy, data):
+        gateway.insert(data)
+        table_proxy_spy.insert_parts.assert_called_once_with(data["parts"])
+
+    def test_if_master_entity_is_inserted_before_part_entities_are(self, gateway, table_proxy_spy, data):
+        table_proxy_spy.insert_master.side_effect = RuntimeError
+        try:
+            gateway.insert(data)
+        except RuntimeError:
+            pass
+        table_proxy_spy.insert_parts.assert_not_called()
+
+
+class TestDelete:
+    def test_if_call_to_translator_is_correct(self, gateway, translator_spy):
+        gateway.delete("identifier0")
+        translator_spy.to_primary_key.assert_called_once_with("identifier0")
+
+    def test_if_call_to_table_proxy_is_correct(self, gateway, table_proxy_spy):
+        gateway.delete("identifier0")
+        table_proxy_spy.delete.assert_called_once_with("primary_key0")
+
+
+class TestSetFlag:
+    @pytest.fixture
+    def flag_name(self):
+        return "my_awesome_flag"
+
+    @pytest.fixture(params=[True, False])
+    def flag_value(self, request):
+        return request.param
+
+    @pytest.fixture(autouse=True)
+    def set_flag(self, gateway, flag_name, flag_value):
+        gateway.set_flag("identifier0", flag_name, flag_value)
+
+    @pytest.fixture
+    def called_method(self, flag_value):
+        if flag_value:
+            return "enable_flag"
+        else:
+            return "disable_flag"
+
+    def test_if_call_to_translator_is_correct(self, translator_spy):
+        translator_spy.to_primary_key.assert_called_once_with("identifier0")
+
+    def test_if_call_to_table_proxy_is_correct(self, table_proxy_spy, called_method):
+        getattr(table_proxy_spy, called_method).assert_called_once_with("primary_key0", "MyAwesomeFlag")
+
+
+def test_if_transaction_is_started_in_proxy(gateway, table_proxy_spy):
+    gateway.start_transaction()
+    table_proxy_spy.start_transaction.assert_called_once_with()
+
+
+def test_if_transaction_is_committed_in_proxy(gateway, table_proxy_spy):
+    gateway.commit_transaction()
+    table_proxy_spy.commit_transaction.assert_called_once_with()
+
+
+def test_if_transaction_is_cancelled_in_proxy(gateway, table_proxy_spy):
+    gateway.cancel_transaction()
+    table_proxy_spy.cancel_transaction.assert_called_once_with()
+
+
+def test_repr(gateway):
+    assert repr(gateway) == "DataJointGateway(table_proxy=table_proxy_spy, translator=translator_spy)"
