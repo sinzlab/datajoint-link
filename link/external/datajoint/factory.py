063d4300c797879f6f9d44127c4cb8d6547392dd
christoph <chris24.blessing@gmail.com>
2020-07-28 12:04:18 +0200

    Update type annotations

--- link/external/datajoint/factory.py
+++ link/external/datajoint/factory.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
-from typing import Optional, List, Dict, Type, Any
+from typing import Optional, List, Dict, Type, Any, Union
 from dataclasses import dataclass
 
-from datajoint import Schema, Lookup, Part
+from datajoint import Schema, Lookup, Part, Table
 
 from ..dj_helpers import get_part_table_classes
 
@@ -26,7 +26,7 @@ class TableFactory:
         self.spawn_table_config: Optional[SpawnTableConfig] = None
         self.create_table_config: Optional[CreateTableConfig] = None
 
-    def __call__(self) -> Type:
+    def __call__(self) -> Union[Type[Lookup], Type[Table]]:
         if self.spawn_table_config is None:
             raise RuntimeError
         try:
@@ -38,7 +38,7 @@ class TableFactory:
         return table_cls
 
     @property
-    def part_tables(self) -> Dict[Part]:
+    def part_tables(self) -> Dict[str, Type[Part]]:
         return {
             name: part_table
             for name, part_table in get_part_table_classes(
@@ -47,49 +47,51 @@ class TableFactory:
         }
 
     @property
-    def flag_tables(self) -> Dict[Part]:
+    def flag_tables(self) -> Dict[str, Type[Part]]:
         return {name: getattr(self(), name) for name in self.spawn_table_config.flag_table_names}
 
-    def _spawn_table_cls(self) -> Type:
+    def _spawn_table_cls(self) -> Union[Type[Lookup], Type[Table]]:
         spawned_table_classes = dict()
         self.spawn_table_config.schema.spawn_missing_classes(context=spawned_table_classes)
         table_cls = spawned_table_classes[self.spawn_table_config.table_name]
         self._set_attrs_on_table_cls(table_cls)
         return table_cls
 
-    def _create_table_cls(self) -> Type:
+    def _create_table_cls(self) -> Type[Lookup]:
         table_cls = self._create_master_table_cls()
         table_cls_attrs = self._create_table_cls_attrs()
         self._set_attrs_on_table_cls(table_cls, table_cls_attrs)
         return self.spawn_table_config.schema(table_cls)
 
-    def _create_master_table_cls(self) -> Type:
+    def _create_master_table_cls(self) -> Type[Lookup]:
+        # noinspection PyTypeChecker
         return type(
             self.spawn_table_config.table_name, (Lookup,), dict(definition=self.create_table_config.table_definition),
         )
 
-    def _create_table_cls_attrs(self) -> Dict[str, Type]:
+    def _create_table_cls_attrs(self) -> Dict[str, Type[Part]]:
         table_cls_attrs = dict()
         self._create_flag_table_classes(table_cls_attrs)
         self._create_non_flag_part_table_classes(table_cls_attrs)
         return table_cls_attrs
 
-    def _create_flag_table_classes(self, table_cls_attrs: Dict[str, Type]) -> None:
+    def _create_flag_table_classes(self, table_cls_attrs: Dict[str, Type[Part]]) -> None:
         table_cls_attrs.update(
             self._create_part_table_classes({name: "-> master" for name in self.spawn_table_config.flag_table_names})
         )
 
-    def _create_non_flag_part_table_classes(self, table_cls_attrs: Dict[str, Type]) -> None:
+    def _create_non_flag_part_table_classes(self, table_cls_attrs: Dict[str, Type[Part]]) -> None:
         table_cls_attrs.update(self._create_part_table_classes(self.create_table_config.part_table_definitions))
 
-    def _create_part_table_classes(self, definitions: Dict[str, str]) -> Dict[str, Type]:
+    def _create_part_table_classes(self, definitions: Dict[str, str]) -> Dict[str, Type[Part]]:
         part_tables = dict()
         for name, definition in definitions.items():
             part_tables[name] = self._create_part_table_cls(name, definition)
         return part_tables
 
     @staticmethod
-    def _create_part_table_cls(name: str, definition: str) -> Type:
+    def _create_part_table_cls(name: str, definition: str) -> Type[Part]:
+        # noinspection PyTypeChecker
         return type(name, (Part,), dict(definition=definition))
 
     def _set_attrs_on_table_cls(self, table_cls: Type, additional_table_cls_attrs: Optional[Dict[str, Any]] = None):
