5a5829a68dcb74c6fcbac0cd84cb1a22253ab00a
christoph <chris24.blessing@gmail.com>
2020-07-30 18:22:13 +0200

    Implement Base class that handles the representation of instances

--- link/entities/representation.py
+++ link/entities/representation.py
@@ -1,4 +1,18 @@
 from typing import Any, List
+from inspect import signature
+
+
+class Base:
+    def __repr__(self) -> str:
+        """Returns a string representation of the instance.
+
+        The representation is constructed from the names and values of all instance attributes whose names are also the
+        names of parameters in the __init__ method of the class.
+        """
+        init_params = signature(self.__class__.__init__).parameters
+        valid_attrs = {name: value for name, value in self.__dict__.items() if name in init_params}
+        attrs_representation = ", ".join(name + "=" + repr(value) for name, value in valid_attrs.items())
+        return self.__class__.__name__ + "(" + attrs_representation + ")"
 
 
 def represent(instance: Any, attrs: List[str]) -> str:
--- tests/unit/entities/test_represent.py
+++ tests/unit/entities/test_represent.py
@@ -1,4 +1,4 @@
-from link.entities.representation import represent
+from link.entities.representation import represent, Base
 
 
 def test_represent():
@@ -8,3 +8,14 @@ def test_represent():
         another_attr = 5
 
     assert represent(SomeClass(), ["some_attr", "another_attr"]) == "SomeClass(some_attr='some_value', another_attr=5)"
+
+
+def test_repr():
+    class Employee(Base):
+        def __init__(self, name, age, is_human=True):
+            self.name = name
+            self.age = age
+            self.is_human = is_human
+            self.is_bald = False
+
+    assert repr(Employee("John", 23)) == "Employee(name='John', age=23, is_human=True)"
