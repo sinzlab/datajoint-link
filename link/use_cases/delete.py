b2e99c59f95c822391d35d41cf3b8de1892ff235
christoph <chris24.blessing@gmail.com>
2020-08-18 14:05:26 +0200

    Implement delete use-case

---
+++ link/use_cases/delete.py
@@ -0,0 +1,34 @@
+from typing import List, Tuple, Set
+
+from .base import UseCase
+from . import RepositoryLink
+
+
+class Delete(UseCase):
+    def execute(self, repo_link: RepositoryLink, identifiers: List[str]) -> None:
+        """Executes logic associated with the deletion of entities from the local repository."""
+        deletion_requested, deletion_not_requested = self._group_by_deletion_requested(repo_link, identifiers)
+        self._approve_deletion(repo_link, deletion_requested)
+        self._delete_from_outbound(repo_link, deletion_not_requested)
+        self._delete_from_local(repo_link, identifiers)
+
+    @staticmethod
+    def _group_by_deletion_requested(repo_link: RepositoryLink, identifiers: List[str]) -> Tuple[Set[str], Set[str]]:
+        deletion_requested = set(i for i in identifiers if repo_link.outbound.flags[i]["deletion_requested"])
+        deletion_not_requested = set(identifiers) - deletion_requested
+        return deletion_requested, deletion_not_requested
+
+    @staticmethod
+    def _approve_deletion(repo_link: RepositoryLink, deletion_requested: Set[str]) -> None:
+        for identifier in deletion_requested:
+            repo_link.outbound.flags[identifier]["deletion_approved"] = True
+
+    @staticmethod
+    def _delete_from_outbound(repo_link: RepositoryLink, deletion_not_requested: Set[str]) -> None:
+        for identifier in deletion_not_requested:
+            del repo_link.outbound[identifier]
+
+    @staticmethod
+    def _delete_from_local(repo_link: RepositoryLink, identifiers: List[str]) -> None:
+        for identifier in identifiers:
+            del repo_link.local[identifier]
---
+++ tests/unit/use_cases/test_delete.py
@@ -0,0 +1,83 @@
+from unittest.mock import MagicMock, create_autospec, call
+from itertools import compress
+
+import pytest
+
+from link.entities.flag_manager import FlagManager
+from link.use_cases.delete import Delete
+from link.use_cases.base import UseCase
+from link.use_cases import RepositoryLink, RepositoryLinkFactory
+
+
+def test_if_subclass_of_use_case():
+    assert issubclass(Delete, UseCase)
+
+
+@pytest.fixture
+def identifiers():
+    return ["identifier" + str(i) for i in range(3)]
+
+
+@pytest.fixture
+def deletion_requested():
+    return [False, True, False]
+
+
+@pytest.fixture
+def flag_manager_spies(deletion_requested):
+    spies = []
+    for flag in deletion_requested:
+        spy = create_autospec(FlagManager, instance=True)
+        spy.__getitem__.return_value = flag
+        spies.append(spy)
+    return spies
+
+
+@pytest.fixture
+def repo_link_spy(identifiers, flag_manager_spies):
+    repo_link_spy = create_autospec(RepositoryLink, instance=True)
+    repo_link_spy.outbound.flags = {i: fms for i, fms in zip(identifiers, flag_manager_spies)}
+    return repo_link_spy
+
+
+@pytest.fixture
+def repo_link_factory_stub(repo_link_spy):
+    return create_autospec(RepositoryLinkFactory, instance=True, return_value=repo_link_spy)
+
+
+@pytest.fixture
+def dummy_output_port():
+    return MagicMock(name="dummy_output_port")
+
+
+@pytest.fixture
+def use_case(repo_link_factory_stub, dummy_output_port):
+    return Delete(repo_link_factory_stub, dummy_output_port)
+
+
+def test_if_deletion_requested_flag_is_checked_in_flag_managers(use_case, identifiers, flag_manager_spies):
+    use_case(identifiers)
+    for spy in flag_manager_spies:
+        spy.__getitem__.assert_called_once_with("deletion_requested")
+
+
+def test_if_deletion_is_approved_on_entities_that_had_it_requested(
+    use_case, identifiers, deletion_requested, flag_manager_spies
+):
+    use_case(identifiers)
+    for spy in compress(flag_manager_spies, deletion_requested):
+        spy.__setitem__.assert_called_once_with("deletion_approved", True)
+
+
+def test_if_entities_that_had_their_deletion_not_requested_are_deleted_from_outbound_repository(
+    use_case, identifiers, deletion_requested, repo_link_spy
+):
+    use_case(identifiers)
+    repo_link_spy.outbound.__delitem__.assert_has_calls(
+        [call(i) for i in compress(identifiers, [not f for f in deletion_requested])], any_order=True
+    )
+
+
+def test_if_all_entities_are_deleted_from_local_repository(use_case, identifiers, repo_link_spy):
+    use_case(identifiers)
+    assert repo_link_spy.local.__delitem__.call_args_list == [call(i) for i in identifiers]
