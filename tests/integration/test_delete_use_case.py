a46da5770e89f31f9de4aff8c43bc9f949a8ac77
christoph <chris24.blessing@gmail.com>
2020-08-19 17:28:49 +0200

    Refactor some fixtures

--- tests/integration/test_delete_use_case.py
+++ tests/integration/test_delete_use_case.py
@@ -7,35 +7,6 @@ USE_CASE = "delete"
 
 
 @pytest.fixture
-def outbound_flags(outbound_identifiers):
-    flags = {i: dict(deletion_requested=False, deletion_approved=False) for i in outbound_identifiers}
-    flags[outbound_identifiers[0]]["deletion_requested"] = True
-    flags[outbound_identifiers[2]]["deletion_requested"] = True
-    return flags
-
-
-@pytest.fixture
-def local_flags(outbound_flags):
-    return {i: dict(deletion_requested=f["deletion_requested"]) for i, f in outbound_flags.items()}
-
-
-@pytest.fixture
-def all_flags(outbound_flags, local_flags):
-    return dict(outbound=outbound_flags, local=local_flags)
-
-
-@pytest.fixture
-def gateway_link_spy(gateway_link_spy, all_flags):
-    for kind, kind_flags in all_flags.items():
-
-        def get_flags(identifier, flags=kind_flags):
-            return flags[identifier]
-
-        getattr(gateway_link_spy, kind).get_flags.side_effect = get_flags
-    return gateway_link_spy
-
-
-@pytest.fixture
 def to_be_deleted_identifiers(local_identifiers):
     return local_identifiers[:3]
 
@@ -43,6 +14,28 @@ def to_be_deleted_identifiers(local_identifiers):
 @pytest.fixture(autouse=True)
 def execute_delete(use_case, to_be_deleted_identifiers):
     use_case(to_be_deleted_identifiers)
+
+
+@pytest.fixture
+def all_flags(outbound_identifiers):
+    outbound_flags = {i: dict(deletion_requested=False, deletion_approved=False) for i in outbound_identifiers}
+    outbound_flags[outbound_identifiers[0]]["deletion_requested"] = True
+    outbound_flags[outbound_identifiers[2]]["deletion_requested"] = True
+    local_flags = {i: dict(deletion_requested=f["deletion_requested"]) for i, f in outbound_flags.items()}
+    return dict(outbound=outbound_flags, local=local_flags)
+
+
+@pytest.fixture
+def gateway_link_spy(gateway_link_spy, all_flags):
+    def make_get_flags(flags):
+        def get_flags(identifier):
+            return flags[identifier]
+
+        return get_flags
+
+    for name, repo_flags in all_flags.items():
+        getattr(gateway_link_spy, name).get_flags.side_effect = make_get_flags(repo_flags)
+    return gateway_link_spy
 
 
 def test_if_entities_that_had_their_deletion_requested_have_it_approved(gateway_link_spy, outbound_identifiers):
--- tests/integration/test_pull_use_case.py
+++ tests/integration/test_pull_use_case.py
@@ -8,9 +8,9 @@ from link.entities.abstract_gateway import AbstractEntityDTO
 USE_CASE = "pull"
 
 
-@pytest.fixture
-def to_be_pulled_identifiers(identifiers):
-    return identifiers[2:7]
+@pytest.fixture(autouse=True)
+def execute_pull(use_case, identifiers):
+    use_case(identifiers[2:7])
 
 
 @pytest.fixture
@@ -24,28 +24,19 @@ def pulled_data(pulled_identifiers):
 
 
 @pytest.fixture
-def pulled_outbound_data(pulled_data):
-    return {identifier: data.create_identifier_only_copy.return_value for identifier, data in pulled_data.items()}
-
-
-@pytest.fixture
 def gateway_link_spy(gateway_link_spy, pulled_data):
     gateway_link_spy.source.fetch.side_effect = pulled_data.values()
     return gateway_link_spy
 
 
-@pytest.fixture(autouse=True)
-def execute_pull(use_case, to_be_pulled_identifiers):
-    use_case(to_be_pulled_identifiers)
-
-
 def test_if_correct_data_is_fetched_from_source_gateway(gateway_link_spy, pulled_identifiers):
-    assert gateway_link_spy.source.fetch.call_args_list == [call(identifier) for identifier in pulled_identifiers]
+    assert gateway_link_spy.source.fetch.call_args_list == [call(i) for i in pulled_identifiers]
 
 
-def test_if_correct_data_is_inserted_into_outbound_gateway(gateway_link_spy, pulled_outbound_data):
-    assert gateway_link_spy.outbound.insert.call_args_list == [call(data) for data in pulled_outbound_data.values()]
+def test_if_correct_data_is_inserted_into_outbound_gateway(gateway_link_spy, pulled_data):
+    pulled_outbound_data = {i: d.create_identifier_only_copy.return_value for i, d in pulled_data.items()}
+    assert gateway_link_spy.outbound.insert.call_args_list == [call(d) for d in pulled_outbound_data.values()]
 
 
 def test_if_correct_data_is_inserted_into_local_gateway(gateway_link_spy, pulled_data):
-    assert gateway_link_spy.local.insert.call_args_list == [call(data) for data in pulled_data.values()]
+    assert gateway_link_spy.local.insert.call_args_list == [call(d) for d in pulled_data.values()]
