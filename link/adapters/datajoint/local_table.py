29c1f9f66268b0062baa288508f8d3e3d0123513
christoph <chris24.blessing@gmail.com>
2020-08-20 11:31:02 +0200

    Add delete method to local table controller

--- link/adapters/datajoint/local_table.py
+++ link/adapters/datajoint/local_table.py
@@ -1,18 +1,25 @@
 from .gateway import DataJointGateway
 from ...use_cases.pull import Pull
+from ...use_cases.delete import Delete
 
 
 class LocalTableController:
     """Controls the execution of local-table-related use-cases."""
 
     pull_use_case: Pull = None
-    delete_use_case = None
+    delete_use_case: Delete = None
     source_gateway: DataJointGateway = None
+    local_gateway: DataJointGateway = None
 
     def pull(self, restriction) -> None:
-        """Pulls the requested entries from the source_repo_cls table into the local table."""
+        """Pulls the requested entities from the source table into the local table."""
         identifiers = self.source_gateway.get_identifiers_in_restriction(restriction)
         self.pull_use_case(identifiers)
+
+    def delete(self, restriction) -> None:
+        """Deletes the requested entities from the local table."""
+        identifiers = self.local_gateway.get_identifiers_in_restriction(restriction)
+        self.delete_use_case(identifiers)
 
     def __repr__(self) -> str:
         return self.__class__.__qualname__ + "()"
--- tests/unit/adapters/datajoint/test_local_table.py
+++ tests/unit/adapters/datajoint/test_local_table.py
@@ -1,7 +1,9 @@
-from unittest.mock import MagicMock
+from unittest.mock import create_autospec
 
 import pytest
 
+from link.adapters.datajoint.gateway import DataJointGateway
+from link.use_cases import USE_CASES
 from link.adapters.datajoint import local_table
 
 
@@ -14,30 +16,52 @@ def test_if_pull_use_case_is_none():
     assert local_table.LocalTableController.pull_use_case is None
 
 
+def test_if_delete_use_case_is_none():
+    assert local_table.LocalTableController.delete_use_case is None
+
+
 def test_if_source_gateway_is_none():
     assert local_table.LocalTableController.source_gateway is None
 
 
-@pytest.fixture
-def pull_use_case():
-    return MagicMock(name="pull_use_case")
+def test_if_local_gateway_is_none():
+    assert local_table.LocalTableController.local_gateway is None
 
 
 @pytest.fixture
-def source_gateway(identifiers):
-    source_gateway = MagicMock(name="source_gateway")
+def pull_use_case_spy():
+    return create_autospec(USE_CASES["pull"], instance=True)
+
+
+@pytest.fixture
+def delete_use_case_spy():
+    return create_autospec(USE_CASES["delete"], instance=True)
+
+
+@pytest.fixture
+def source_gateway_spy(identifiers):
+    source_gateway = create_autospec(DataJointGateway, instance=True)
     source_gateway.get_identifiers_in_restriction.return_value = identifiers
     return source_gateway
 
 
 @pytest.fixture
-def controller_cls(pull_use_case, source_gateway):
+def local_gateway_spy(identifiers):
+    local_gateway = create_autospec(DataJointGateway, instance=True)
+    local_gateway.get_identifiers_in_restriction.return_value = identifiers
+    return local_gateway
+
+
+@pytest.fixture
+def controller_cls(pull_use_case_spy, delete_use_case_spy, source_gateway_spy, local_gateway_spy):
     class LocalTableController(local_table.LocalTableController):
         pass
 
     LocalTableController.__qualname__ = LocalTableController.__name__
-    LocalTableController.pull_use_case = pull_use_case
-    LocalTableController.source_gateway = source_gateway
+    LocalTableController.pull_use_case = pull_use_case_spy
+    LocalTableController.delete_use_case = delete_use_case_spy
+    LocalTableController.source_gateway = source_gateway_spy
+    LocalTableController.local_gateway = local_gateway_spy
     return LocalTableController
 
 
@@ -46,14 +70,26 @@ def controller(controller_cls):
     return controller_cls()
 
 
-def test_if_restriction_is_converted_into_identifiers(source_gateway, controller, restriction):
-    controller.pull(restriction)
-    source_gateway.get_identifiers_in_restriction.assert_called_once_with(restriction)
+class TestPull:
+    def test_if_restriction_is_converted_into_identifiers(self, source_gateway_spy, controller, restriction):
+        controller.pull(restriction)
+        source_gateway_spy.get_identifiers_in_restriction.assert_called_once_with(restriction)
+
+    def test_if_pull_use_case_is_called_with_identifiers(self, identifiers, pull_use_case_spy, controller, restriction):
+        controller.pull(restriction)
+        pull_use_case_spy.assert_called_once_with(identifiers)
 
 
-def test_if_pull_use_case_is_called_with_identifiers(identifiers, pull_use_case, controller, restriction):
-    controller.pull(restriction)
-    pull_use_case.assert_called_once_with(identifiers)
+class TestDelete:
+    def test_if_restriction_is_converted_into_identifiers(self, local_gateway_spy, controller, restriction):
+        controller.delete(restriction)
+        local_gateway_spy.get_identifiers_in_restriction.assert_called_once_with(restriction)
+
+    def test_if_delete_use_case_is_called_with_identifiers(
+        self, identifiers, delete_use_case_spy, controller, restriction
+    ):
+        controller.delete(restriction)
+        delete_use_case_spy.assert_called_once_with(identifiers)
 
 
 def test_repr(controller):
