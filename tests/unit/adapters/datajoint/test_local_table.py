654fcf82e691fb739e5e6cbcd0fa4a8442320ef9
christoph <chris24.blessing@gmail.com>
2020-08-20 12:31:55 +0200

    Refactor tests

--- tests/unit/adapters/datajoint/test_local_table.py
+++ tests/unit/adapters/datajoint/test_local_table.py
@@ -8,78 +8,73 @@ from link.use_cases import USE_CASES
 from link.adapters.datajoint import local_table
 
 
-@pytest.fixture
-def restriction():
-    return "restriction"
-
-
 def test_if_subclass_of_base():
     assert issubclass(local_table.LocalTableController, Base)
 
 
 @pytest.fixture
-def pull_use_case_spy():
-    return create_autospec(USE_CASES["pull"], instance=True)
+def restriction():
+    return "restriction"
 
 
 @pytest.fixture
-def delete_use_case_spy():
-    return create_autospec(USE_CASES["delete"], instance=True)
+def use_case_spies():
+    return {n: create_autospec(USE_CASES[n], instance=True) for n in ["pull", "delete"]}
 
 
 @pytest.fixture
-def source_gateway_spy(identifiers):
-    source_gateway = create_autospec(DataJointGateway, instance=True)
-    source_gateway.get_identifiers_in_restriction.return_value = identifiers
-    return source_gateway
+def gateway_spies(identifiers):
+    spies = {}
+    for name in ["source", "local"]:
+        spy = create_autospec(DataJointGateway, instance=True)
+        spy.get_identifiers_in_restriction.return_value = identifiers
+        spies[name] = spy
+    return spies
 
 
 @pytest.fixture
-def local_gateway_spy(identifiers):
-    local_gateway = create_autospec(DataJointGateway, instance=True)
-    local_gateway.get_identifiers_in_restriction.return_value = identifiers
-    return local_gateway
-
-
-@pytest.fixture
-def controller(pull_use_case_spy, delete_use_case_spy, source_gateway_spy, local_gateway_spy):
+def controller(use_case_spies, gateway_spies):
     class LocalTableController(local_table.LocalTableController):
         pass
 
-    return LocalTableController(pull_use_case_spy, delete_use_case_spy, source_gateway_spy, local_gateway_spy)
+    return LocalTableController(
+        use_case_spies["pull"], use_case_spies["delete"], gateway_spies["source"], gateway_spies["local"]
+    )
 
 
 class TestInit:
-    def test_if_pull_use_case_is_stored_as_instance_attribute(self, controller, pull_use_case_spy):
-        assert controller.pull_use_case is pull_use_case_spy
+    def test_if_pull_use_case_is_stored_as_instance_attribute(self, controller, use_case_spies):
+        assert controller.pull_use_case is use_case_spies["pull"]
 
-    def test_if_delete_use_case_is_stored_as_instance_attribute(self, controller, delete_use_case_spy):
-        assert controller.delete_use_case is delete_use_case_spy
+    def test_if_delete_use_case_is_stored_as_instance_attribute(self, controller, use_case_spies):
+        assert controller.delete_use_case is use_case_spies["delete"]
 
-    def test_if_source_gateway_is_stored_as_instance_attribute(self, controller, source_gateway_spy):
-        assert controller.source_gateway is source_gateway_spy
+    def test_if_source_gateway_is_stored_as_instance_attribute(self, controller, gateway_spies):
+        assert controller.source_gateway is gateway_spies["source"]
 
-    def test_if_local_gateway_is_stored_as_instance_attribute(self, controller, local_gateway_spy):
-        assert controller.local_gateway is local_gateway_spy
-
-
-class TestPull:
-    def test_if_restriction_is_converted_into_identifiers(self, source_gateway_spy, controller, restriction):
-        controller.pull(restriction)
-        source_gateway_spy.get_identifiers_in_restriction.assert_called_once_with(restriction)
-
-    def test_if_pull_use_case_is_called_with_identifiers(self, identifiers, pull_use_case_spy, controller, restriction):
-        controller.pull(restriction)
-        pull_use_case_spy.assert_called_once_with(identifiers)
+    def test_if_local_gateway_is_stored_as_instance_attribute(self, controller, gateway_spies):
+        assert controller.local_gateway is gateway_spies["local"]
 
 
-class TestDelete:
-    def test_if_restriction_is_converted_into_identifiers(self, local_gateway_spy, controller, restriction):
-        controller.delete(restriction)
-        local_gateway_spy.get_identifiers_in_restriction.assert_called_once_with(restriction)
+class TestMethod:
+    @pytest.fixture(params=["pull", "delete"])
+    def method_name(self, request):
+        return request.param
 
-    def test_if_delete_use_case_is_called_with_identifiers(
-        self, identifiers, delete_use_case_spy, controller, restriction
-    ):
-        controller.delete(restriction)
-        delete_use_case_spy.assert_called_once_with(identifiers)
+    @pytest.fixture(autouse=True)
+    def execute(self, controller, method_name, restriction):
+        getattr(controller, method_name)(restriction)
+
+    @pytest.fixture
+    def gateway_spy(self, gateway_spies, method_name):
+        return {"pull": gateway_spies["source"], "delete": gateway_spies["local"]}[method_name]
+
+    @pytest.fixture
+    def use_case_spy(self, use_case_spies, method_name):
+        return use_case_spies[method_name]
+
+    def test_if_restriction_is_converted_into_identifiers(self, gateway_spy, restriction):
+        gateway_spy.get_identifiers_in_restriction.assert_called_once_with(restriction)
+
+    def test_if_use_case_is_called_with_identifiers(self, identifiers, use_case_spy):
+        use_case_spy.assert_called_once_with(identifiers)
