7ab6a201e1c19d3bc2fbceab7859f1ddd0da7032
christoph <chris24.blessing@gmail.com>
2020-08-21 17:03:24 +0200

    Replace dict functions with literals

--- link/external/datajoint/factory.py
+++ link/external/datajoint/factory.py
@@ -57,7 +57,7 @@ class TableFactory(Base):
         return {name: getattr(self(), name) for name in self.config.flag_table_names}
 
     def _spawn_table_cls(self) -> Type[UserTable]:
-        spawned_table_classes = dict()
+        spawned_table_classes = {}
         self.config.schema.spawn_missing_classes(context=spawned_table_classes)
         table_cls = spawned_table_classes[self.config.table_name]
         return self._extend_table_cls(table_cls)
@@ -66,7 +66,7 @@ class TableFactory(Base):
         self, table_cls: Type[UserTable], part_table_classes: Optional[Dict[str, Type[Part]]] = None
     ) -> Type[UserTable]:
         if part_table_classes is None:
-            part_table_classes = dict()
+            part_table_classes = {}
         if self.config.table_definition:
             table_cls_attrs = dict(self.config.table_cls_attrs, definition=self.config.table_definition)
         else:
@@ -77,7 +77,7 @@ class TableFactory(Base):
         )
 
     def _create_table_cls(self) -> Type[UserTable]:
-        part_table_classes = dict()
+        part_table_classes = {}
         self._create_flag_part_table_classes(part_table_classes)
         self._create_non_flag_part_table_classes(part_table_classes)
         extended_table_cls = self._extend_table_cls(self.config.table_cls, part_table_classes)
@@ -92,7 +92,7 @@ class TableFactory(Base):
         part_table_classes.update(self._create_part_table_classes(self.config.part_table_definitions))
 
     def _create_part_table_classes(self, definitions: Dict[str, str]) -> Dict[str, Type[Part]]:
-        part_tables = dict()
+        part_tables = {}
         for name, definition in definitions.items():
             part_tables[name] = self._create_part_table_cls(name, definition)
         return part_tables
