170c2fb69c57bfada6c12101ead14e1f90235380
christoph <chris24.blessing@gmail.com>
2020-08-25 12:02:30 +0200

    Implement response model for pull use-case

--- link/use_cases/pull.py
+++ link/use_cases/pull.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Set
+from dataclasses import dataclass
 
 from .base import UseCase
 
@@ -16,3 +17,24 @@ class PullUseCase(UseCase):
             for entity in entities:
                 repo_link.outbound[entity.identifier] = entity.create_identifier_only_copy()
                 repo_link.local[entity.identifier] = entity
+
+
+@dataclass
+class PullResponseModel:
+    """Response model for the pull use-case."""
+
+    requested: Set[str]
+    valid: Set[str]
+    invalid: Set[str]
+
+    @property
+    def n_requested(self) -> int:
+        return len(self.requested)
+
+    @property
+    def n_valid(self) -> int:
+        return len(self.valid)
+
+    @property
+    def n_invalid(self) -> int:
+        return len(self.invalid)
---
+++ tests/unit/use_cases/test_pull_response_model.py
@@ -0,0 +1,41 @@
+from dataclasses import is_dataclass
+
+import pytest
+
+from link.use_cases.pull import PullResponseModel
+
+
+def test_if_dataclass():
+    assert is_dataclass(PullResponseModel)
+
+
+@pytest.fixture
+def requested():
+    return {"identifier" + str(i) for i in range(10)}
+
+
+@pytest.fixture
+def valid():
+    return {"identifier" + str(i) for i in range(5)}
+
+
+@pytest.fixture
+def invalid():
+    return {"identifier" + str(i) for i in range(5, 10)}
+
+
+@pytest.fixture
+def model(requested, valid, invalid):
+    return PullResponseModel(requested=requested, valid=valid, invalid=invalid)
+
+
+def test_n_requested_property(model, requested):
+    assert model.n_requested == len(requested)
+
+
+def test_n_valid_property(model, valid):
+    assert model.n_valid == len(valid)
+
+
+def test_n_invalid_property(model, invalid):
+    assert model.n_invalid == len(invalid)
