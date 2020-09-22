e008f7e7a66cce66d8f851983a35abddf50c8696
christoph <chris24.blessing@gmail.com>
2020-09-22 10:52:06 +0200

    Use more abstract type annotations for function/method arguments

--- link/frameworks/datajoint/dj_helpers.py
+++ link/frameworks/datajoint/dj_helpers.py
@@ -1,6 +1,6 @@
 import re
 import warnings
-from typing import Dict, Type, List, Optional
+from typing import Dict, Type, Optional, Collection
 from inspect import isclass
 
 from datajoint import Part
@@ -23,7 +23,7 @@ def replace_stores(definition: str, stores: Dict[str, str]) -> str:
 
 
 def get_part_table_classes(
-    table_cls: Type[UserTable], ignored_parts: Optional[List[str]] = None
+    table_cls: Type[UserTable], ignored_parts: Optional[Collection[str]] = None
 ) -> Dict[str, Type[Part]]:
     if ignored_parts is None:
         ignored_parts = []
