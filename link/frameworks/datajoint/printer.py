d9699b44e2f43e4cc8740e8155f6b41dd4124ed5
christoph <chris24.blessing@gmail.com>
2020-09-30 12:37:36 +0200

    Add some docstrings and refactor to improve readability

--- link/frameworks/datajoint/printer.py
+++ link/frameworks/datajoint/printer.py
@@ -3,20 +3,31 @@ from ...adapters.datajoint.presenter import ViewModel
 
 
 class Printer(Base):
+    """View that uses Python's built-in print function to display information."""
+
     def __init__(self, view_model: ViewModel) -> None:
         self.view_model = view_model
 
     def __call__(self) -> None:
-        width = self._compute_width()
-        field_lines = [k + ":" + str(v).rjust(width)[len(k) + 1 :] for k, v in self.view_model.fields.items()]
-        lines = ["=" * width, self.view_model.message.center(width), "-" * width] + field_lines + ["=" * width]
+        """Formats and prints the data contained in the view model."""
+        field_lines = [k + ":" + str(v).rjust(self._width)[len(k) + 1 :] for k, v in self.view_model.fields.items()]
+        lines = (
+            ["=" * self._width, self.view_model.message.center(self._width), "-" * self._width]
+            + field_lines
+            + ["=" * self._width]
+        )
         print("\n".join(lines))
 
-    def _compute_width(self) -> int:
-        message_width = len(self.view_model.message)
-        max_field_width = max(len(k + str(v)) + 2 for k, v in self.view_model.fields.items())
-        if message_width >= max_field_width:
-            return message_width + 2
-        if (max_field_width - message_width) % 2 != 0:
-            return max_field_width + 1
-        return max_field_width
+    @property
+    def _width(self) -> int:
+        """Computes and returns the width of the printed output."""
+        if len(self.view_model.message) >= self._max_field_length:
+            return len(self.view_model.message) + 2
+        if (self._max_field_length - len(self.view_model.message)) % 2 != 0:
+            return self._max_field_length + 1
+        return self._max_field_length
+
+    @property
+    def _max_field_length(self) -> int:
+        """Computes and returns the length of the longest field."""
+        return max(len(k + str(v)) + 2 for k, v in self.view_model.fields.items())
