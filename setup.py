8a228b63c02b8246f3a401cf71bd8458748e3643
Chris <chris.blessing@protonmail.com>
2020-10-05 16:15:00 +0200

    Update name, email, description, url and classifiers

--- setup.py
+++ setup.py
@@ -4,16 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="link",
+    name="datajoint-link",
     version="0.0.1",
     author="Christoph Blessing",
-    author_email="chris24.blessing@gmail.com",
-    description="A package that lets you link DataJoint schemas",
+    author_email="chris.blessing@protonmail.com",
+    description="A tool for linking two DataJoint tables located on different database servers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cblessing24/link",
+    url="https://github.com/sinzlab/link",
     packages=setuptools.find_packages(),
-    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Database",
+    ],
     python_requires=">=3.8",
     install_requires=["datajoint"],
 )
