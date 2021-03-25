3a0344ac9d7513b8f77e6e2da6e675ba9a5a307c
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-03-25 15:37:22 +0100

    Avoid explicit naming of use-case names

--- dj_link/__init__.py
+++ dj_link/__init__.py
@@ -5,7 +5,7 @@ from .frameworks.datajoint import initialize_frameworks
 from .frameworks.datajoint.link import Link, LocalTableMixin  # noqa: F401
 from .frameworks.datajoint.printer import Printer
 from .schemas import LazySchema  # noqa: F401
-from .use_cases import REQUEST_MODELS, initialize_use_cases
+from .use_cases import REQUEST_MODELS, USE_CASES, initialize_use_cases
 
 _REPO_NAMES = ("source", "outbound", "local")
 
@@ -13,14 +13,8 @@ _REPO_NAMES = ("source", "outbound", "local")
 def _initialize() -> None:
     facade_link = initialize_frameworks(_REPO_NAMES)
     gateway_link, view_model, presenter = initialize_adapters(facade_link)
-    initialized_use_cases = initialize_use_cases(
-        gateway_link,
-        dict(
-            pull=presenter.pull,
-            delete=presenter.delete,
-            refresh=presenter.refresh,
-        ),
-    )
+    output_ports = {name: getattr(presenter, name) for name in USE_CASES}
+    initialized_use_cases = initialize_use_cases(gateway_link, output_ports)
     LocalTableMixin.controller = Controller(initialized_use_cases, REQUEST_MODELS, gateway_link)
     LocalTableMixin.printer = Printer(view_model)
 
