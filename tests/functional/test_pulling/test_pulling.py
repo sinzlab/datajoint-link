361a1227e76566de8edbf66e2ac6befc685dbf67
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-02-15 16:18:17 +0000

    Define users in test_pulling2

--- tests/functional/test_pulling/test_pulling.py
+++ tests/functional/test_pulling/test_pulling.py
@@ -5,6 +5,8 @@ import pytest
 
 from dj_link import LazySchema, Link
 
+from ..conftest import UserConfig
+
 USES_EXTERNAL = False
 
 
@@ -13,12 +15,32 @@ def test_pulling(pulled_data, expected_data):
     assert pulled_data == expected_data
 
 
-def test_pulling2(get_conn, src_db_spec, local_db_spec):
-    expected = [{"foo": 1, "bar": "a"}, {"foo": 2, "bar": "b"}]
-    with get_conn(src_db_spec, src_db_spec.config.users["end_user"]) as connection:
-        source_schema = dj.schema("end_user_schema", connection=connection)
+def test_pulling2(get_conn, source_db, local_db, create_user):
+    local_schema_name = "awesome"
+    source_schema_name = "cool"
+    outbound_schema_name = "cool_outbound"
 
-        @source_schema
+    source_user = UserConfig(
+        "John", "apples", grants=[f"GRANT ALL PRIVILEGES ON `{source_schema_name}`.* TO 'John'@'%';"]
+    )
+    link_user = UserConfig(
+        "Link",
+        "bananas",
+        grants=[
+            f"GRANT SELECT, REFERENCES ON `{source_schema_name}`.* TO 'Link'@'%';",
+            f"GRANT ALL PRIVILEGES ON `{outbound_schema_name}`.* TO 'Link'@'%';",
+        ],
+    )
+    local_user = UserConfig("Amy", "pears", grants=[f"GRANT ALL PRIVILEGES ON `{local_schema_name}`.* TO 'Amy'@'%';"])
+
+    create_user(source_db, source_user)
+    create_user(source_db, link_user)
+    create_user(local_db, local_user)
+
+    expected = [{"foo": 1, "bar": "a"}, {"foo": 2, "bar": "b"}]
+    with get_conn(source_db, source_user) as connection:
+
+        @dj.schema(source_schema_name, connection=connection)
         class SomeTable(dj.Manual):
             definition = """
             foo: int
@@ -28,16 +50,18 @@ def test_pulling2(get_conn, src_db_spec, local_db_spec):
 
         SomeTable().insert(expected)
 
-    os.environ["LINK_USER"] = src_db_spec.config.users["dj_user"].name
-    os.environ["LINK_PASS"] = src_db_spec.config.users["dj_user"].password
-    os.environ["LINK_OUTBOUND"] = "outbound_schema"
+    os.environ["LINK_USER"] = link_user.name
+    os.environ["LINK_PASS"] = link_user.password
+    os.environ["LINK_OUTBOUND"] = outbound_schema_name
 
-    dj.config["database.host"] = local_db_spec.container.name
-    dj.config["database.user"] = local_db_spec.config.users["end_user"].name
-    dj.config["database.password"] = local_db_spec.config.users["end_user"].password
+    dj.config["database.host"] = local_db.container.name
+    dj.config["database.user"] = local_user.name
+    dj.config["database.password"] = local_user.password
 
-    local_schema = LazySchema("end_user_local_schema")
-    source_schema = LazySchema("end_user_schema", host=src_db_spec.container.name)
+    dj.conn(reset=True)
+
+    local_schema = LazySchema(local_schema_name)
+    source_schema = LazySchema(source_schema_name, host=source_db.container.name)
 
     @Link(local_schema, source_schema)
     class SomeTable(dj.Manual):
