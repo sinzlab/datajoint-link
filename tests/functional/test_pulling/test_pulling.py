dc3527656de252f5ec14363b279359b84f86ed48
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-02-20 14:55:18 +0000

    Update basic pulling test
    - Use random strings for schema names
    - Use context manager for setting local database connection

--- tests/functional/test_pulling/test_pulling.py
+++ tests/functional/test_pulling/test_pulling.py
@@ -21,10 +21,8 @@ def temp_env_var(name, value):
             del os.environ[name]
 
 
-def test_pulling(get_conn, source_db, local_db, create_user):
-    local_schema_name = "awesome"
-    source_schema_name = "cool"
-    outbound_schema_name = "cool_outbound"
+def test_pulling(create_random_string, get_conn, source_db, local_db, create_user):
+    local_schema_name, source_schema_name, outbound_schema_name = (create_random_string() for _ in range(3))
 
     source_user = create_user(source_db, grants=[f"GRANT ALL PRIVILEGES ON `{source_schema_name}`.* TO '$name'@'%';"])
     link_user = create_user(
@@ -49,13 +47,9 @@ def test_pulling(get_conn, source_db, local_db, create_user):
 
         SomeTable().insert(expected)
 
-    dj.config["database.host"] = local_db.container.name
-    dj.config["database.user"] = local_user.name
-    dj.config["database.password"] = local_user.password
-
-    with temp_env_var("LINK_USER", link_user.name), temp_env_var("LINK_PASS", link_user.password), temp_env_var(
-        "LINK_OUTBOUND", outbound_schema_name
-    ):
+    with get_conn(local_db, local_user), temp_env_var("LINK_USER", link_user.name), temp_env_var(
+        "LINK_PASS", link_user.password
+    ), temp_env_var("LINK_OUTBOUND", outbound_schema_name):
         local_schema = LazySchema(local_schema_name)
         source_schema = LazySchema(source_schema_name, host=source_db.container.name)
 
