1975673330d0c3f4f86e34b4170dc175e027d53b
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-02-21 10:20:25 +0000

    Add failing test testing source attribute

--- tests/functional/test_pulling/test_pulling.py
+++ tests/functional/test_pulling/test_pulling.py
@@ -65,3 +65,31 @@ def test_pulling(create_random_string, create_table, get_conn, source_db, local_
         local_table_cls().pull()
         actual = local_table_cls().fetch(as_dict=True)
         assert actual == expected
+
+
+@pytest.mark.xfail
+def test_if_source_attributes_of_different_local_tables_differ(
+    create_random_string, create_user, source_db, local_db, create_table, get_conn
+):
+    local_schema_name, source_schema_name, outbound_schema_name = (create_random_string() for _ in range(3))
+
+    source_user = create_user(source_db, grants=[f"GRANT ALL PRIVILEGES ON `{source_schema_name}`.* TO '$name'@'%';"])
+    link_user = create_user(
+        source_db,
+        grants=[
+            f"GRANT SELECT, REFERENCES ON `{source_schema_name}`.* TO '$name'@'%';",
+            f"GRANT ALL PRIVILEGES ON `{outbound_schema_name}`.* TO '$name'@'%';",
+        ],
+    )
+    local_user = create_user(local_db, grants=[f"GRANT ALL PRIVILEGES ON `{local_schema_name}`.* TO '$name'@'%';"])
+
+    source_table_names = (create_table(source_db, source_user, source_schema_name, "foo: int\n---") for _ in range(2))
+
+    with get_conn(local_db, local_user), temp_env_var("LINK_USER", link_user.name), temp_env_var(
+        "LINK_PASS", link_user.password
+    ), temp_env_var("LINK_OUTBOUND", outbound_schema_name):
+        local_schema = LazySchema(local_schema_name)
+        source_schema = LazySchema(source_schema_name, host=source_db.container.name)
+        link = Link(local_schema, source_schema)
+        local_table_cls1, local_table_cls2 = (link(type(name, (dj.Manual,), {})) for name in source_table_names)
+        assert local_table_cls1().source.full_table_name != local_table_cls2().source.full_table_name
