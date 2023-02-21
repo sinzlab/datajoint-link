6f3b2dbad2258f8795881304df03afd04d17fdac
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-02-21 09:57:33 +0000

    Add create_table fixture

--- tests/functional/test_pulling/test_pulling.py
+++ tests/functional/test_pulling/test_pulling.py
@@ -2,6 +2,7 @@ import os
 from contextlib import contextmanager
 
 import datajoint as dj
+import pytest
 
 from dj_link import LazySchema, Link
 
@@ -21,7 +22,23 @@ def temp_env_var(name, value):
             del os.environ[name]
 
 
-def test_pulling(create_random_string, get_conn, source_db, local_db, create_user):
+@pytest.fixture
+def create_table(get_conn, create_random_string):
+    def _create_table(db_spec, user_spec, schema_name, definition, data=None):
+        if data is None:
+            data = []
+        with get_conn(db_spec, user_spec) as connection:
+            table_name = create_random_string().title()
+            table_cls = type(table_name, (dj.Manual,), {"definition": definition})
+            schema = dj.schema(schema_name, connection=connection)
+            schema(table_cls)
+            table_cls().insert(data)
+        return table_name
+
+    return _create_table
+
+
+def test_pulling(create_random_string, create_table, get_conn, source_db, local_db, create_user):
     local_schema_name, source_schema_name, outbound_schema_name = (create_random_string() for _ in range(3))
 
     source_user = create_user(source_db, grants=[f"GRANT ALL PRIVILEGES ON `{source_schema_name}`.* TO '$name'@'%';"])
@@ -35,28 +52,16 @@ def test_pulling(create_random_string, get_conn, source_db, local_db, create_use
     local_user = create_user(local_db, grants=[f"GRANT ALL PRIVILEGES ON `{local_schema_name}`.* TO '$name'@'%';"])
 
     expected = [{"foo": 1, "bar": "a"}, {"foo": 2, "bar": "b"}]
-    with get_conn(source_db, source_user) as connection:
-
-        @dj.schema(source_schema_name, connection=connection)
-        class SomeTable(dj.Manual):
-            definition = """
-            foo: int
-            ---
-            bar: varchar(64)
-            """
-
-        SomeTable().insert(expected)
+    source_table_name = create_table(
+        source_db, source_user, source_schema_name, "foo: int\n---\nbar: varchar(64)", expected
+    )
 
     with get_conn(local_db, local_user), temp_env_var("LINK_USER", link_user.name), temp_env_var(
         "LINK_PASS", link_user.password
     ), temp_env_var("LINK_OUTBOUND", outbound_schema_name):
         local_schema = LazySchema(local_schema_name)
         source_schema = LazySchema(source_schema_name, host=source_db.container.name)
-
-        @Link(local_schema, source_schema)
-        class SomeTable(dj.Manual):
-            pass
-
-        SomeTable().pull()
-        actual = SomeTable().fetch(as_dict=True)
+        local_table_cls = Link(local_schema, source_schema)(type(source_table_name, (dj.Manual,), {}))
+        local_table_cls().pull()
+        actual = local_table_cls().fetch(as_dict=True)
         assert actual == expected
