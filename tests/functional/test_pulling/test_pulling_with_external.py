bd874444462d2214232c9c92946788b01a9493eb
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-03-02 13:40:10 +0000

    Refactor test for pulling with external
    squash! Refactor test for pulling with external

--- tests/functional/test_pulling/test_pulling_with_external.py
+++ tests/functional/test_pulling/test_pulling_with_external.py
@@ -16,22 +16,15 @@ def src_data(src_data, file_paths):
     return [dict(e, ext_attr=f) for e, f in zip(src_data, file_paths)]
 
 
-@pytest.fixture
-def pulled_data(local_table_cls, local_dir, dj_config, local_db_spec, src_store_config, local_store_config):
+@pytest.mark.usefixtures("create_and_cleanup_buckets", "src_table_with_data")
+def test_pulling(
+    local_table_cls, local_dir, dj_config, local_db_spec, src_store_config, local_store_config, expected_data
+):
     with dj_config(
         local_db_spec, local_db_spec.config.users["end_user"], stores=[src_store_config, local_store_config]
     ):
         local_table_cls().pull()
-    return local_table_cls().fetch(as_dict=True, download_path=local_dir)
-
-
-@pytest.fixture
-def expected_data(expected_data, local_dir):
+    pulled_data = local_table_cls().fetch(as_dict=True, download_path=local_dir)
     for entity in expected_data:
         entity["ext_attr"] = os.path.join(local_dir, os.path.basename(entity["ext_attr"]))
-    return expected_data
-
-
-@pytest.mark.usefixtures("create_and_cleanup_buckets", "src_table_with_data")
-def test_pulling(pulled_data, expected_data):
     assert pulled_data == expected_data
