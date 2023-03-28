cee6a92d11c5c5efad1f92206ab6c7689fb86b22
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-03-28 14:49:08 +0200

    Remove some framework tests

--- tests/unit/frameworks/datajoint/test_link.py
+++ tests/unit/frameworks/datajoint/test_link.py
@@ -2,10 +2,7 @@ import os
 from unittest.mock import MagicMock, call
 
 import pytest
-from datajoint import Lookup, Schema
 
-from dj_link.base import Base
-from dj_link.frameworks.datajoint.dj_helpers import replace_stores
 from dj_link.frameworks.datajoint.factory import TableFactory, TableFactoryConfig, TableTiers
 from dj_link.frameworks.datajoint.link import Link
 
@@ -116,32 +113,6 @@ def dummy_cls(table_name):
     return MagicMock(name="dummy_cls", __name__=table_name)
 
 
-def test_if_link_is_subclass_of_base():
-    assert issubclass(Link, Base)
-
-
-def test_if_schema_class_class_attribute_is_datajoint_schema_class():
-    assert Link.schema_cls is Schema
-
-
-def test_if_replace_stores_func_class_attribute_is_replace_stores():
-    assert Link.replace_stores_func is replace_stores
-
-
-class TestInit:
-    def test_if_local_schema_is_stored_as_instance_attribute(self, link, local_schema_stub):
-        assert link.local_schema is local_schema_stub
-
-    def test_if_source_schema_is_stored_as_instance_attribute(self, link, source_schema_stub):
-        assert link.source_schema is source_schema_stub
-
-    def test_if_stores_is_stored_as_instance_attribute(self, link, stores):
-        assert link.stores is stores
-
-    def test_if_stores_is_empty_dict_if_not_provided(self, local_schema_stub, source_schema_stub):
-        assert Link(local_schema_stub, source_schema_stub).stores == dict()
-
-
 @pytest.fixture
 def prepare_env():
     os.environ["LINK_OUTBOUND"] = "outbound_schema"
@@ -191,9 +162,6 @@ class TestCallWithoutInitialSetup:
     def test_if_configuration_of_local_table_cls_factory_is_correct(self, table_cls_factory_spies, basic_local_config):
         assert table_cls_factory_spies["local"].config == TableFactoryConfig(**basic_local_config)
 
-    def test_if_call_to_local_table_cls_factory_is_correct(self, table_cls_factory_spies):
-        table_cls_factory_spies["local"].assert_called_once_with()
-
     def test_if_local_table_class_is_returned(self, linked_table):
         assert linked_table == "local_table_cls"
 
@@ -205,9 +173,6 @@ def initial_setup_required(table_cls_factory_spies):
 
 @pytest.mark.usefixtures("prepare_env", "initial_setup_required", "linked_table")
 class TestCallWithInitialSetup:
-    def test_if_source_table_cls_factory_is_called(self, table_cls_factory_spies):
-        assert table_cls_factory_spies["source"].call_args_list == [call(), call()]
-
     def test_if_configuration_of_outbound_table_cls_factory_is_correct(
         self, table_cls_factory_spies, basic_outbound_config, source_table_cls_stub
     ):
@@ -236,9 +201,6 @@ class TestCallWithInitialSetup:
             part_table_definitions=dict(PartA="replaced_heading", PartB="replaced_heading", PartC="replaced_heading"),
             tier=TableTiers.LOOKUP,
         )
-
-    def test_if_calls_to_local_table_cls_factory_are_correct(self, table_cls_factory_spies):
-        assert table_cls_factory_spies["local"].call_args_list == [call(), call()]
 
     def test_if_local_table_class_is_returned(self, linked_table):
         assert linked_table == "local_table_cls"
