b97d5d208ddd207eec0e0c4fa129822c91d5993e
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-05-04 13:58:46 +0200

    Move test for factory function

--- tests/unit/entities/test_link.py
+++ tests/unit/entities/test_link.py
@@ -8,17 +8,17 @@ import pytest
 from dj_link.entities.link import Components, Identifier, Link, Transfer, create_link, pull
 
 
-def test_can_create_link() -> None:
-    assignments = {
-        Components.SOURCE: {Identifier("1")},
-        Components.OUTBOUND: {Identifier("1")},
-        Components.LOCAL: {Identifier("1")},
-    }
-    link = create_link(assignments)
-    assert link == Link(source={Identifier("1")}, outbound={Identifier("1")}, local={Identifier("1")})
-
-
 class TestCreateLink:
+    @staticmethod
+    def test_can_create_link() -> None:
+        assignments = {
+            Components.SOURCE: {Identifier("1")},
+            Components.OUTBOUND: {Identifier("1")},
+            Components.LOCAL: {Identifier("1")},
+        }
+        link = create_link(assignments)
+        assert link == Link(source={Identifier("1")}, outbound={Identifier("1")}, local={Identifier("1")})
+
     @staticmethod
     @pytest.mark.parametrize(
         "assignments,expectation",
