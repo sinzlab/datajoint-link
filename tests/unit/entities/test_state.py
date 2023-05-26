61e522ad732a8595cc9f4d07ae6c654e008fb088
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-05-26 14:53:44 +0200

    Combine tests for activated entities

--- tests/unit/entities/test_state.py
+++ tests/unit/entities/test_state.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+import pytest
+
 from dj_link.entities import command
 from dj_link.entities.link import create_link
 from dj_link.entities.state import Components, Identifier, Operations
@@ -11,25 +17,22 @@ def test_pulling_idle_entity_returns_correct_commands() -> None:
     assert entity.pull() == {command.AddToOutbound(Identifier("1")), command.StartPullOperation(Identifier("1"))}
 
 
-def test_processing_activated_entity_undergoing_pull_operation_returns_correct_commands() -> None:
+@pytest.mark.parametrize(
+    "operation,commands",
+    [
+        (Operations.PULL, {command.AddToLocal}),
+        (Operations.DELETE, {command.RemoveFromOutbound, command.FinishDeleteOperation}),
+    ],
+)
+def test_processing_activated_entity_returns_correct_commands(
+    operation: Operations, commands: Iterable[type[command.Command]]
+) -> None:
     link = create_link(
         create_assignments({Components.SOURCE: {"1"}, Components.OUTBOUND: {"1"}}),
-        operations={Operations.PULL: {Identifier("1")}},
+        operations={operation: {Identifier("1")}},
     )
     entity = next(iter(link[Components.SOURCE]))
-    assert entity.process() == {command.AddToLocal(Identifier("1"))}
-
-
-def test_processing_activated_entity_undergoing_delete_operation_returns_correct_commands() -> None:
-    link = create_link(
-        create_assignments({Components.SOURCE: {"1"}, Components.OUTBOUND: {"1"}}),
-        operations={Operations.DELETE: {Identifier("1")}},
-    )
-    entity = next(iter(link[Components.SOURCE]))
-    assert entity.process() == {
-        command.RemoveFromOutbound(Identifier("1")),
-        command.FinishDeleteOperation(Identifier("1")),
-    }
+    assert entity.process() == {command(Identifier("1")) for command in commands}
 
 
 def test_processing_received_entity_undergoing_pull_operation_returns_correct_commands() -> None:
