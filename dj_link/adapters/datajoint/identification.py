05151068780fb87b65397376559c737a560a855a
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-06-07 16:57:05 +0200

    Add new uuid based identification translator

--- dj_link/adapters/datajoint/identification.py
+++ dj_link/adapters/datajoint/identification.py
@@ -1,7 +1,10 @@
 """Contains code used to translate between DataJoint primary keys and identifiers."""
+from __future__ import annotations
+
 import hashlib
 import json
-from typing import Dict
+from typing import Dict, Union
+from uuid import UUID, uuid4
 
 from ...base import Base
 from ...custom_types import PrimaryKey
@@ -23,3 +26,21 @@ class IdentificationTranslator(Base):
     def to_primary_key(self, identifier: str) -> PrimaryKey:
         """Translate the provided identifier to its corresponding primary key."""
         return self._identifier_to_primary_key_mapping[identifier]
+
+
+class UUIDIdentificationTranslator:
+    """Translates between DataJoint-specific primary keys and domain-model-specific identifiers."""
+
+    def __init__(self) -> None:
+        """Initialize the translator."""
+        self.__mapping: dict[tuple[tuple[str, Union[str, int, float]], ...], UUID] = {}
+
+    def to_identifier(self, primary_key: PrimaryKey) -> UUID:
+        """Translate the given primary key to its corresponding identifier."""
+        primary_key_tuple = tuple((k, v) for k, v in primary_key.items())
+        return self.__mapping.setdefault(primary_key_tuple, uuid4())
+
+    def to_primary_key(self, identifier: UUID) -> PrimaryKey:
+        """Translate the given identifier to its corresponding primary key."""
+        primary_key_tuple = {v: k for k, v in self.__mapping.items()}[identifier]
+        return dict(primary_key_tuple)
--- tests/unit/adapters/datajoint/test_identification.py
+++ tests/unit/adapters/datajoint/test_identification.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dj_link.adapters.datajoint.identification import IdentificationTranslator
+from dj_link.adapters.datajoint.identification import IdentificationTranslator, UUIDIdentificationTranslator
 from dj_link.base import Base
 
 
@@ -38,3 +38,21 @@ def test_if_returned_identifier_is_key_order_independent(translator, primary_key
 def test_if_correct_primary_key_is_returned(translator, primary_key, identifier):
     translator.to_identifier(primary_key)
     assert translator.to_primary_key(identifier) == primary_key
+
+
+def test_primary_key_translated_to_identifier_and_back_is_identical_to_original():
+    translator = UUIDIdentificationTranslator()
+    primary_key = {"a": 5, "b": 20}
+    assert translator.to_primary_key(translator.to_identifier(primary_key)) == primary_key
+
+
+def test_translating_same_primary_key_multiple_times_yields_same_identifier():
+    translator = UUIDIdentificationTranslator()
+    primary_key = {"saffd": "heasdf", "12": 12}
+    assert len({translator.to_identifier(primary_key) for _ in range(10)}) == 1
+
+
+def test_different_primary_keys_are_translated_to_different_identifiers():
+    translator = UUIDIdentificationTranslator()
+    primary_key1, primary_key2 = {"a": 5, "b": 20}, {"abc": 1.2, "asd": "hello"}
+    assert translator.to_identifier(primary_key1) != translator.to_identifier(primary_key2)
