15101b638a4bf91fc61d0b52cbf4ca8eabaf09ef
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-06-08 15:33:23 +0200

    Remove old identification translator

--- dj_link/adapters/datajoint/identification.py
+++ dj_link/adapters/datajoint/identification.py
@@ -1,33 +1,12 @@
 """Contains code used to translate between DataJoint primary keys and identifiers."""
 from __future__ import annotations
 
-import hashlib
-import json
-from typing import Dict, Union
+from typing import Union
 from uuid import UUID, uuid4
 
 from dj_link.entities.custom_types import Identifier
 
-from ...base import Base
 from ...custom_types import PrimaryKey
-
-
-class IdentificationTranslator(Base):
-    """Used to translate between DataJoint primary keys and identifiers."""
-
-    def __init__(self) -> None:
-        """Initialize the identification translator."""
-        self._identifier_to_primary_key_mapping: Dict[str, PrimaryKey] = {}
-
-    def to_identifier(self, primary_key: PrimaryKey) -> str:
-        """Translate the provided primary key to its corresponding identifier."""
-        identifier = hashlib.blake2b(json.dumps(primary_key, sort_keys=True).encode()).hexdigest()
-        self._identifier_to_primary_key_mapping[identifier] = primary_key
-        return identifier
-
-    def to_primary_key(self, identifier: str) -> PrimaryKey:
-        """Translate the provided identifier to its corresponding primary key."""
-        return self._identifier_to_primary_key_mapping[identifier]
 
 
 class UUIDIdentificationTranslator:
--- tests/unit/adapters/datajoint/test_identification.py
+++ tests/unit/adapters/datajoint/test_identification.py
@@ -1,43 +1,4 @@
-import pytest
-
-from dj_link.adapters.datajoint.identification import IdentificationTranslator, UUIDIdentificationTranslator
-from dj_link.base import Base
-
-
-@pytest.fixture
-def primary_key():
-    return dict(a=5, b=20)
-
-
-@pytest.fixture
-def identifier():
-    return (
-        "ea420704c495156e41aaeda7c5f58301ea85d98e0003e914b1deedc6fd2f"
-        "b19dbcd79bb10f4d3631c45373f67f0df16927e0da879ab99d1a7df59eefb7f77031"
-    )
-
-
-@pytest.fixture
-def translator():
-    return IdentificationTranslator()
-
-
-def test_if_identification_translator_is_subclass_of_base():
-    assert issubclass(IdentificationTranslator, Base)
-
-
-def test_if_correct_identifier_is_returned(translator, primary_key, identifier):
-    assert translator.to_identifier(primary_key) == identifier
-
-
-def test_if_returned_identifier_is_key_order_independent(translator, primary_key, identifier):
-    reversed_key_order = {k: primary_key[k] for k in reversed(primary_key)}
-    assert translator.to_identifier(reversed_key_order) == identifier
-
-
-def test_if_correct_primary_key_is_returned(translator, primary_key, identifier):
-    translator.to_identifier(primary_key)
-    assert translator.to_primary_key(identifier) == primary_key
+from dj_link.adapters.datajoint.identification import UUIDIdentificationTranslator
 
 
 def test_primary_key_translated_to_identifier_and_back_is_identical_to_original():
