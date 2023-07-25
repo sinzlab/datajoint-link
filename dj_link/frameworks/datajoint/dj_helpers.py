29ee48c7d07cd40e68884a7ff825ebef881e6b20
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-07-25 13:54:27 +0200

    Improve replace stores function

--- dj_link/frameworks/datajoint/dj_helpers.py
+++ dj_link/frameworks/datajoint/dj_helpers.py
@@ -1,6 +1,9 @@
 """Contains DataJoint helper functions."""
+from __future__ import annotations
+
 import re
 import warnings
+from collections.abc import Mapping
 from inspect import isclass
 from typing import Collection, Dict, Optional, Type
 
@@ -8,18 +11,20 @@ from datajoint import Part
 from datajoint.user_tables import UserTable
 
 
-def replace_stores(definition: str, stores: Dict[str, str]) -> str:
+def replace_stores(definition: str, stores: Mapping[str, str]) -> str:
     """Replace the store in the definition according to a mapping of replacement to original stores."""
     stores = {original: replacement for replacement, original in stores.items()}
 
     def replace_store(match):
         try:
-            return match.groups()[0] + stores[match.groups()[1]]
+            return match.group("prefix") + stores[match.group("original")]
         except KeyError:
-            warnings.warn(f"No replacement for store '{match.groups()[1]}' specified. Skipping!", category=UserWarning)
+            warnings.warn(
+                f"No replacement for store '{match.group('original')}' specified. Skipping!", category=UserWarning
+            )
             return match.group(0)
 
-    pattern = re.compile(r"(attach@)(\S+)")
+    pattern = re.compile(r"(?P<prefix>attach@)(?P<original>\S+)")
     return re.sub(pattern, replace_store, definition)
 
 
