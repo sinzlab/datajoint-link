a75a0bfa79aa6bff4a447c3ffbf85e1cad31b8f3
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-08-02 16:34:31 +0200

    Add datajoint controller

--- dj_link/adapters/datajoint/controller.py
+++ dj_link/adapters/datajoint/controller.py
@@ -1,10 +1,17 @@
 """Contains code controlling the execution of use-cases."""
-from typing import Mapping
+from __future__ import annotations
+
+from typing import Callable, Iterable, Mapping
+
+from dj_link.custom_types import PrimaryKey
+from dj_link.entities.custom_types import Identifier
+from dj_link.use_cases.use_cases import UseCases
 
 from ...base import Base
 from ...use_cases import RequestModelClasses
 from ...use_cases.base import AbstractUseCase
 from .gateway import DataJointGatewayLink
+from .identification import IdentificationTranslator
 
 
 class Controller(Base):
@@ -36,3 +43,24 @@ class Controller(Base):
     def refresh(self) -> None:
         """Refresh the repositories."""
         self.use_cases["refresh"](self.request_model_classes["refresh"]())
+
+
+class DJController:
+    """Controls the execution of use-cases from DataJoint."""
+
+    def __init__(
+        self,
+        handlers: Mapping[UseCases, Callable[[Iterable[Identifier]], None]],
+        translator: IdentificationTranslator,
+    ) -> None:
+        """Initialize the translator."""
+        self.__handlers = handlers
+        self.__translator = translator
+
+    def pull(self, primary_keys: Iterable[PrimaryKey]) -> None:
+        """Execute the pull use-case."""
+        self.__handlers[UseCases.PULL](self.__translator.to_identifiers(primary_keys))
+
+    def delete(self, primary_keys: Iterable[PrimaryKey]) -> None:
+        """Execute the delete use-case."""
+        self.__handlers[UseCases.DELETE](self.__translator.to_identifiers(primary_keys))
