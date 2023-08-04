de624bf176fdb8248c511635a1a2ef16e5eb4ae3
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-08-04 16:59:41 +0200

    Remove obsolete use-cases code

--- dj_link/use_cases/gateway.py
+++ dj_link/use_cases/gateway.py
@@ -4,32 +4,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from collections.abc import Iterable
 
-from ..entities.abstract_gateway import AbstractGateway
-from ..entities.link import Link, Transfer
+from ..entities.link import Link
 from ..entities.state import Update
-
-
-class GatewayLink(ABC):
-    """Contains all three gateways involved in a link."""
-
-    @property
-    @abstractmethod
-    def source(self) -> AbstractGateway:
-        """Return the source gateway."""
-
-    @property
-    @abstractmethod
-    def outbound(self) -> AbstractGateway:
-        """Return the outbound gateway."""
-
-    @property
-    @abstractmethod
-    def local(self) -> AbstractGateway:
-        """Return the local gateway."""
-
-    @abstractmethod
-    def transfer(self, spec: Transfer) -> None:
-        """Transfer an entity from one component in the link to another."""
 
 
 class LinkGateway(ABC):
--- tests/unit/use_cases/response_models/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.fixture()
-def requested(create_identifiers):
-    return set(create_identifiers(10))
