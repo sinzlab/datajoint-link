a5d19a1656762897110188cee2cfdc7521fe8471
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-08-21 14:48:31 +0200

    Remover unnecessary pulling tests

--- tests/functional/test_pulling/test_pulling.py
+++ tests/functional/test_pulling/test_pulling.py
@@ -5,49 +5,7 @@ import datajoint as dj
 from dj_link import link
 
 
-def test_pulling(prepare_link, create_table, connection_config, databases, configured_environment):
-    schema_names, user_specs = prepare_link()
-
-    expected = [{"foo": 1, "bar": "a"}, {"foo": 2, "bar": "b"}]
-    source_table_name = create_table(
-        databases["source"], user_specs["source"], schema_names["source"], "foo: int\n---\nbar: varchar(64)", expected
-    )
-
-    with connection_config(databases["local"], user_specs["local"]), configured_environment(
-        user_specs["link"], schema_names["outbound"]
-    ):
-        local_table_cls = link(databases["source"].container.name, schema_names["source"], schema_names["local"])(
-            type(source_table_name, (dj.Manual,), {})
-        )
-        local_table_cls().pull()
-        actual = local_table_cls().fetch(as_dict=True)
-        assert actual == expected
-
-
-def test_can_pull_into_different_local_tables_from_same_source(
-    prepare_multiple_links, create_table, databases, connection_config, configured_environment
-):
-    schema_names, user_specs = prepare_multiple_links(2)
-
-    expected = [{"foo": 1, "bar": "a"}, {"foo": 2, "bar": "b"}]
-    source_table_name = create_table(
-        databases["source"], user_specs["source"], schema_names["source"], "foo: int\n---\nbar: varchar(64)", expected
-    )
-
-    def fetch(local_schema_name):
-        local_table_cls = link(databases["source"].container.name, schema_names["source"], local_schema_name)(
-            type(source_table_name, (dj.Manual,), {})
-        )
-        local_table_cls().pull()
-        return local_table_cls().fetch(as_dict=True)
-
-    with connection_config(databases["local"], user_specs["local"]), configured_environment(
-        user_specs["link"], schema_names["outbound"]
-    ):
-        assert all(fetch(name) == expected for name in schema_names["local"])
-
-
-def test_pulling_with_external_and_part(
+def test_pulling(
     create_random_string,
     prepare_link,
     tmpdir,
