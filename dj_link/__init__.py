0aae790b2f20c11e04b96789f6466fe7ea231a9d
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-08-23 18:24:05 +0200

    Remove lazy schema

--- dj_link/__init__.py
+++ dj_link/__init__.py
@@ -1,5 +1,4 @@
 """A tool for linking two DataJoint tables located on different database servers."""
 from .frameworks.link import create_link as link
-from .schemas import LazySchema
 
-__all__ = ["LazySchema", "link"]
+__all__ = ["link"]
