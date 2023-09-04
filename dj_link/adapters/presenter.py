550a242dd935465115452ee72d68d794bdd55cc1
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-09-04 16:42:23 +0200

    Do not rename response models on import

--- dj_link/adapters/presenter.py
+++ dj_link/adapters/presenter.py
@@ -1,14 +1,13 @@
 """Contains the presenter class and related classes/functions."""
 
-from dj_link.use_cases.use_cases import DeleteResponseModel as DJDeleteResponseModel
-from dj_link.use_cases.use_cases import PullResponseModel as DJPullResponseModel
+from dj_link.use_cases.use_cases import DeleteResponseModel, PullResponseModel
 
 
 class DJPresenter:
     """DataJoint-specific presenter."""
 
-    def pull(self, response: DJPullResponseModel) -> None:
+    def pull(self, response: PullResponseModel) -> None:
         """Present information about a finished pull use-case."""
 
-    def delete(self, response: DJDeleteResponseModel) -> None:
+    def delete(self, response: DeleteResponseModel) -> None:
         """Present information about a finished delete use-case."""
