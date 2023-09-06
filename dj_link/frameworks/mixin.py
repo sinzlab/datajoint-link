4347c4e111b756ec0e9143fbe99186b89bc4e818
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-09-06 16:24:26 +0200

    Add in transit attribute to source endpoint

--- dj_link/frameworks/mixin.py
+++ dj_link/frameworks/mixin.py
@@ -15,8 +15,7 @@ from . import DJTables
 class SourceEndpoint(Protocol):
     """Protocol for the source endpoint."""
 
-    def process(self) -> None:
-        """Process all transiting entities."""
+    in_transit: TransitMixin
 
 
 class LocalMixin:
@@ -30,7 +29,7 @@ class LocalMixin:
         """Delete pulled entities from the local table."""
         primary_keys = self.proj().fetch(as_dict=True)
         self._controller.delete(primary_keys)
-        self.source.process()
+        self.source.in_transit.process()
 
     @property
     def source(self) -> SourceEndpoint:
@@ -47,6 +46,7 @@ class SourceMixin:
     """Mixin class for the source endpoint."""
 
     proj: Callable[[], dj.Table]
+    in_transit: TransitMixin
     _controller: DJController
     _outbound_table: Callable[[], dj.Table]
     __and__: Callable[[SourceMixin, dj.Table], dj.Table]
@@ -55,22 +55,12 @@ class SourceMixin:
         """Pull idle entities from the source table into the local table."""
         primary_keys = self.proj().fetch(as_dict=True)
         self._controller.pull(primary_keys)
-        self.process()
-
-    def process(self) -> None:
-        """Process all transiting entities."""
-        while primary_keys := self.in_transit.proj().fetch(as_dict=True):
-            self._controller.process(primary_keys)
+        self.in_transit.process()
 
     @property
     def flagged(self) -> Sequence[PrimaryKey]:
         """Return the primary keys of all flagged entities."""
         return (self._outbound_table() & "is_flagged = 'TRUE'").proj().fetch(as_dict=True)
-
-    @property
-    def in_transit(self) -> dj.Table:
-        """Return the source table restricted to all entities that are currently undergoing a process."""
-        return self & (self._outbound_table() & "process != 'NONE'")
 
 
 def create_source_endpoint_factory(
@@ -85,11 +75,48 @@ def create_source_endpoint_factory(
             type(
                 source_table_cls.__name__,
                 (SourceMixin, source_table_cls),
-                {"_controller": controller, "_outbound_table": staticmethod(outbound_table)},
+                {
+                    "_controller": controller,
+                    "_outbound_table": staticmethod(outbound_table),
+                    "in_transit": create_transit_endpoint(controller, source_table(), outbound_table()),
+                },
             )(),
         )
 
     return create_source_endpoint
+
+
+class TransitEndpoint(Protocol):
+    """Protocol for the transit endpoint."""
+
+    def process(self) -> None:
+        """Process all transiting entities."""
+
+    def __and__(self, condition: dj.Table) -> TransitEndpoint:
+        """Restrict the endpoint with the given condition."""
+
+
+class TransitMixin:
+    """Mixin class used to create the transit endpoint when combined with the source table."""
+
+    proj: Callable[[], dj.Table]
+    _controller: DJController
+
+    def process(self) -> None:
+        """Process all transiting entities."""
+        while primary_keys := self.proj().fetch(as_dict=True):
+            self._controller.process(primary_keys)
+
+
+def create_transit_endpoint(
+    controller: DJController, source_table: dj.Table, outbound_table: dj.Table
+) -> TransitEndpoint:
+    """Create the endpoint responsible for transiting entities."""
+    in_transit_cls = cast(
+        "type[TransitEndpoint]",
+        type(type(source_table).__name__, (TransitMixin, type(source_table)), {"_controller": controller}),
+    )
+    return in_transit_cls() & (outbound_table & "process != 'NONE'")
 
 
 def create_local_endpoint(controller: DJController, tables: DJTables) -> dj.Table:
