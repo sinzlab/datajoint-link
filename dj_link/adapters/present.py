1ddfd22b42003bd463892e1fc19dd71e39537789
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-09-14 15:58:30 +0200

    Mention entity state on failed operation

--- dj_link/adapters/present.py
+++ dj_link/adapters/present.py
@@ -53,12 +53,16 @@ class Failure:
 
     primary_key: PrimaryKey
     operation: str
+    state: str
 
 
 def create_operation_response_presenter(
     translator: IdentificationTranslator, show: Callable[[OperationRecord], None]
 ) -> Callable[[OperationResponse], None]:
     """Create a callable that when called presents information about a finished operation."""
+
+    def get_class_name(obj: type) -> str:
+        return obj.__name__
 
     def present_operation_response(response: OperationResponse) -> None:
         show(
@@ -72,14 +76,19 @@ def create_operation_response_presenter(
                         translator.to_primary_key(update.identifier),
                         operation=response.operation.name,
                         transition=Transition(
-                            update.transition.current.__name__.upper(), update.transition.new.__name__.upper()
+                            get_class_name(update.transition.current).upper(),
+                            get_class_name(update.transition.new).upper(),
                         ),
                     )
                     for update in response.updates
                 ],
                 [
-                    Failure(translator.to_primary_key(operation.identifier), operation.operation.name)
-                    for operation in response.errors
+                    Failure(
+                        translator.to_primary_key(error.identifier),
+                        error.operation.name,
+                        get_class_name(error.state).upper(),
+                    )
+                    for error in response.errors
                 ],
             )
         )
