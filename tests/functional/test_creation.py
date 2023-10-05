9255c40c6fa7569a63896e149eb5c0afde2d7e7a
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-10-05 16:17:35 +0200

    Test local table creation with CURRENT_TIMESTAMP

--- tests/functional/test_creation.py
+++ tests/functional/test_creation.py
@@ -1,4 +1,5 @@
 import datajoint as dj
+import pytest
 
 from link import link
 
@@ -18,6 +19,26 @@ def test_local_table_creation_from_source_table_that_has_parent_raises_no_error(
         source_table,
         context={"source_table_parent": source_table_parent},
     )
+    with connection_config(databases["local"], user_specs["local"]), configured_environment(
+        user_specs["link"], schema_names["outbound"]
+    ):
+        link(
+            databases["source"].container.name,
+            schema_names["source"],
+            schema_names["outbound"],
+            "Outbound",
+            schema_names["local"],
+        )(type(source_table_name, (dj.Manual,), {}))
+
+
+@pytest.mark.xfail()
+def test_local_table_creation_from_source_table_that_uses_current_timestamp_default_raises_no_error(
+    prepare_link, create_table, prepare_table, databases, configured_environment, connection_config
+):
+    schema_names, user_specs = prepare_link()
+    source_table_name = "Foo"
+    source_table = create_table(source_table_name, dj.Manual, "foo = CURRENT_TIMESTAMP : timestamp")
+    prepare_table(databases["source"], user_specs["source"], schema_names["source"], source_table)
     with connection_config(databases["local"], user_specs["local"]), configured_environment(
         user_specs["link"], schema_names["outbound"]
     ):
