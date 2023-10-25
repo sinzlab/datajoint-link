7e8d778d5b4e92b7888ee8a0e20409fd0e11bae3
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-10-25 14:58:46 +0200

    Add commands for starting processes

--- link/domain/commands.py
+++ link/domain/commands.py
@@ -10,9 +10,19 @@ from .custom_types import Identifier
 class Command:
     """Base class for all commands."""
 
+    requested: frozenset[Identifier]
+
 
 @dataclass(frozen=True)
 class ProcessLink(Command):
     """Process the requested entities in the link."""
 
-    requested: frozenset[Identifier]
+
+@dataclass(frozen=True)
+class StartPullProcess(Command):
+    """Start the pull process for the requested entities."""
+
+
+@dataclass(frozen=True)
+class StartDeleteProcess(Command):
+    """Start the delete process for the requested entities."""
